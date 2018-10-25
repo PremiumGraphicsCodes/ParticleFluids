66cca8852d3b171298e03ccd085845e406297b58
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2018-10-25 14:12:17 +0900

    add GUI.

---
+++ Crystal/PGWPF/Canvas3dView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace PGWPF
+{
+    /// <summary>
+    /// Canvas3dView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Canvas3dView : UserControl
+    {
+        public Canvas3dView()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
