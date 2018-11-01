70516ff8ddbfdc4412c8bd3247ff13b2df30d12b
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2018-11-01 14:27:21 +0900

    change VS2017 -> VS2015.

---
+++ Crystal/PGCLI/ObjectRepositoryAdapter.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "ObjectRepositoryAdapter.h"
+#include "../../Crystal/UI/ObjectRepository.h"
+
+/*
+using namespace PGCLI;
+
+ObjectRepositoryAdapter::ObjectRepositoryAdapter()
+{}
+
+ObjectRepositoryAdapter::~ObjectRepositoryAdapter()
+{}
+
+int ObjectRepositoryAdapter::AddWireFrameObject()
+{
+	return 0;
+}
+*/
---
+++ Crystal/PGCLI/ObjectRepositoryAdapter.h
@@ -0,0 +1,27 @@
+#pragma once
+/*
+using namespace System;
+
+namespace Crystal {
+	namespace UI {
+		class ModelRepository;
+	}
+}
+
+namespace PGCLI {
+
+	public ref class ObjectRepositoryAdapter
+	{
+	public:
+		ObjectRepositoryAdapter();
+
+		~ObjectRepositoryAdapter(void);
+
+		int AddWireFrameObject();
+
+	internal:
+		Crystal::UI::ModelRepository* instance;
+	};
+
+}
+*/
