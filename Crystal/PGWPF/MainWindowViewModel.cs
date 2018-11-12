93b3d2434fea3b4c2fe24efd0fec828a6875e289
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2018-11-12 10:14:14 +0900

    add viewmodel.

---
+++ Crystal/PGWPF/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PGWPF
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            ;
+        }
+
+        public class SphereGenerationCommandImpl : ICommand
+        {
+            public event EventHandler CanExecuteChanged
+            {
+                add { }
+                remove { }
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                ;
+            }
+        }
+
+    }
+}
