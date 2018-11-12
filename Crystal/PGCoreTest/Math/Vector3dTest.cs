e6ea0a6b227631b8cd5444d0165bd03dc8082f55
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2018-11-12 10:51:33 +0900

    add test project.

---
+++ Crystal/PGCoreTest/Math/Vector3dTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PGWPF.Math;
+
+namespace PGCoreTest
+{
+    [TestClass]
+    public class Vector3dTest
+    {
+        [TestMethod]
+        public void TestLengthSquard()
+        {
+            var vector = new Vector3d(10.0, 0.0, 0.0);
+            Assert.AreEqual( vector.LengthSquared, 100.0 );
+        }
+
+        [TestMethod]
+        public void TestLength()
+        {
+            var vector = new Vector3d(10.0, 0.0, 0.0);
+            Assert.AreEqual(vector.Length, 10.0);
+        }
+    }
+}
---
+++ Crystal/PGCoreTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PGCoreTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PGCoreTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef5360be-7dbe-4197-ad64-55dd61acd8f7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
