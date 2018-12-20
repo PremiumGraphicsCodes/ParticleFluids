d448ef2d21176e849166f89ac79ae8ef4cf5638b
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2018-12-20 15:53:38 +0900

    add views.

---
+++ Crystal/PGWPF/Generation/ParticleSystem/CylinderGenerationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using PG.Control;
+using Prism.Mvvm;
+using Reactive.Bindings;
+
+namespace PG.CGStudio.Generation.ParticleSystem
+{
+    public class CylinderGenerationViewModel : BindableBase
+    {
+        public ReactiveProperty<int> Count { get; }
+
+        public Cylinder3dViewModel CylinderViewModel { get; }
+
+        public ReactiveCommand GenerationCommand { get; }
+
+        public CylinderGenerationViewModel()
+        {
+            this.Count = new ReactiveProperty<int>(10000);
+            this.CylinderViewModel = new Cylinder3dViewModel();
+            this.GenerationCommand = new ReactiveCommand();
+            this.GenerationCommand.Subscribe(OnGenerate);
+        }
+
+        private void OnGenerate()
+        {
+            ;
+        }
+    }
+}
--- Crystal/PGWPF/Generation/PolygonMesh/PolygonMeshGenerationViewModel.cs
+++ Crystal/PGWPF/Generation/PolygonMesh/PolygonMeshGenerationViewModel.cs
@@ -4,11 +4,8 @@ namespace PG.CGStudio.Generation.PolygonMesh
 {
     public class PolygonMeshGenerationViewModel : BindableBase
     {
-        public SphereGenerationViewModel SphereGenerationViewModel { get; private set; }
-
         public PolygonMeshGenerationViewModel()
         {
-            SphereGenerationViewModel = new SphereGenerationViewModel();
         }
     }
 }
