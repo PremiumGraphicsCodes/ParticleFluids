d6b7d6314be439ada9fe1f507b1eac36eb0f524a
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-02-13 17:20:26 +0900

    refactoring.

--- Crystal/Shape/Particle.h
+++ Crystal/Shape/Particle.h
@@ -7,8 +7,23 @@
 namespace Crystal {
 	namespace Shape {
 
+class IParticle
+{
+public:
+	~IParticle()
+	{}
+
+	virtual Math::Vector3dd getPosition() const = 0;
+
+	virtual void move(const Math::Vector3dd& v) = 0;
+
+	virtual void transform(const Math::Matrix3dd& m) = 0;
+
+	virtual void transform(const Math::Matrix4dd& m) = 0;
+};
+
 template<typename Attr>
-class Particle
+class Particle : public IParticle
 {
 public:
 	Particle(const Math::Vector3dd& position, const Attr& attr) :
@@ -16,13 +31,13 @@ public:
 		attribute(attr)
 	{}
 
-	Math::Vector3dd getPosition() const { return position; }
+	Math::Vector3dd getPosition() const override { return position; }
 
-	void move(const Math::Vector3dd& v) { this->position += v; }
+	void move(const Math::Vector3dd& v) override { this->position += v; }
 
-	void transform(const Math::Matrix3dd& m) { position = m * position; }
+	void transform(const Math::Matrix3dd& m) override { position = m * position; }
 
-	void transform(const Math::Matrix4dd& m) { position = m * glm::vec4(position, 1.0); }
+	void transform(const Math::Matrix4dd& m) override { position = m * glm::vec4(position, 1.0); }
 
 	Attr getAttribute() const { return attribute; }
 
