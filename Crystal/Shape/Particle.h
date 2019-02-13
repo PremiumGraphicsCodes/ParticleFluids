7d2af9046bcc93404dbfc4643e643830e878899a
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-02-13 17:37:47 +0900

    refactoring.

--- Crystal/Shape/Particle.h
+++ Crystal/Shape/Particle.h
@@ -1,26 +1,9 @@
 #pragma once
 
-#include "../Math/Vector3d.h"
-#include "../Math/Matrix3d.h"
-#include "../Math/Matrix4d.h"
+#include "IParticle.h"
 
 namespace Crystal {
 	namespace Shape {
-
-class IParticle
-{
-public:
-	~IParticle()
-	{}
-
-	virtual Math::Vector3dd getPosition() const = 0;
-
-	virtual void move(const Math::Vector3dd& v) = 0;
-
-	virtual void transform(const Math::Matrix3dd& m) = 0;
-
-	virtual void transform(const Math::Matrix4dd& m) = 0;
-};
 
 template<typename Attr>
 class Particle : public IParticle
