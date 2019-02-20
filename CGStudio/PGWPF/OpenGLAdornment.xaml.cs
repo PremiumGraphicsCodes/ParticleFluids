7b6bfc33664faa4e9b16c18d0ecceaad550d13ae
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-02-20 10:10:54 +0900

    use hwnd-adorner.

---
+++ CGStudio/PGWPF/OpenGLAdornment.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PG.CGStudio
+{
+    public partial class OpenGLAdornment : System.Windows.Controls.UserControl
+    {
+        public OpenGLAdornment()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CGStudio/PGWPF/OpenGLPresenter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using HwndExtensions.Host;
+
+namespace PG.CGStudio
+{
+    public class OpenGLPresenter : HwndHostPresenter
+    {
+        public OpenGLPresenter()
+        {
+            var browser = new WebBrowser();
+            //browser.Source = new Uri("http://blogs.microsoft.co.il/michaels/2014/11/28/wpf-hwnd-adorner/");
+
+            HwndHost = browser;
+            RegisterToAppShutdown();
+        }
+
+        private void RegisterToAppShutdown()
+        {
+            Application.Current.Dispatcher.ShutdownStarted += OnShutdownStarted;
+        }
+
+        private void OnShutdownStarted(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                var host = HwndHost;
+                if (host != null)
+                    host.Dispose();
+            }
+        }        
+    }
+}
