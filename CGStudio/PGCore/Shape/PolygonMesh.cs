4a39525d856826a261965efa7e94c89022432966
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-03-14 11:03:16 +0900

    add views.

--- CGStudio/PGCore/Shape/PolygonMesh.cs
+++ CGStudio/PGCore/Shape/PolygonMesh.cs
@@ -3,16 +3,6 @@ using System.Collections.Generic;
 
 namespace PG.Core.Shape
 {
-    public class Vertex
-    {
-        public Vertex(Vector3d position)
-        {
-            this.Position = position;
-        }
-
-        public Vector3d Position { get; set; }
-    }
-
     public class Face
     {
         public Face(Vertex v0, Vertex v1, Vertex v2)
---
+++ CGStudio/PGCore/Shape/Vertex.cs
@@ -0,0 +1,14 @@
+﻿using PG.Core.Math;
+
+namespace PG.Core.Shape
+{
+    public class Vertex
+    {
+        public Vertex(Vector3d position)
+        {
+            this.Position = position;
+        }
+
+        public Vector3d Position { get; set; }
+    }
+}
