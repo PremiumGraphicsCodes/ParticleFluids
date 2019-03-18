d3313870fb033339d3eb2ab0e07a37731fc0276a
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-03-18 15:03:55 +0900

    add view.

---
+++ CGStudio/PGWPF/Texture/TextureListViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.CGStudio.Texture
+{
+    public class TreeSource : INotifyPropertyChanged
+    {
+        private bool _IsExpanded = true;
+        private string _Text = "";
+        private TreeSource _Parent = null;
+        private ObservableCollection<TreeSource> _Children = null;
+
+
+        public bool IsExpanded
+        {
+            get { return _IsExpanded; }
+            set { _IsExpanded = value; OnPropertyChanged("IsExpanded"); }
+        }
+
+        public string Text
+        {
+            get { return _Text; }
+            set { _Text = value; OnPropertyChanged("Text"); }
+        }
+
+        public TreeSource Parent
+        {
+            get { return _Parent; }
+            set { _Parent = value; OnPropertyChanged("Parent"); }
+        }
+
+        public ObservableCollection<TreeSource> Children
+        {
+            get { return _Children; }
+            set { _Children = value; OnPropertyChanged("Childen"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string name)
+        {
+            if (null == this.PropertyChanged) return;
+            this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        public void Add(TreeSource child)
+        {
+            if (null == Children) Children = new ObservableCollection<TreeSource>();
+            child.Parent = this;
+            Children.Add(child);
+        }
+    }
+
+    public class TextureListViewModel
+    {
+    }
+}
---
+++ CGStudio/PGWPF/Texture/TextureListViewl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PG.CGStudio.Texture
+{
+    /// <summary>
+    /// TextureListViewModel.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TextureListView : UserControl
+    {
+        public TextureListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
