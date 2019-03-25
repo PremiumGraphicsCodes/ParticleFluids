cfe1bf81fd27c3c5d95ad38102375a459e93658f
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-03-25 10:18:05 +0900

    add menu.

--- CGStudio/PGWPF/Generation/ParticleSystem/ParticleSystemGenerationViewModel.cs
+++ CGStudio/PGWPF/Generation/ParticleSystem/ParticleSystemGenerationViewModel.cs
@@ -1,16 +1,11 @@
 ﻿using Prism.Mvvm;
-using Reactive.Bindings;
 
 namespace PG.CGStudio.Generation.ParticleSystem
 {
     public class ParticleSystemGenerationViewModel : BindableBase
     {
-        public ReactiveProperty<BindableBase> ContentViewModel { get; }
-            = new ReactiveProperty<BindableBase>();
-
         public ParticleSystemGenerationViewModel()
         {
-            this.ContentViewModel.Value = new BoxGenerationViewModel();
         }
     }
 }
--- CGStudio/PGWPF/Generation/WireFrame/WireFrameGenerationViewModel.cs
+++ CGStudio/PGWPF/Generation/WireFrame/WireFrameGenerationViewModel.cs
@@ -1,13 +1,9 @@
 ﻿using Prism.Mvvm;
-using Reactive.Bindings;
 
 namespace PG.CGStudio.Generation.WireFrame
 {
     public class WireFrameGenerationViewModel : BindableBase
     {
-        public ReactiveProperty<BindableBase> ContentViewModel { get; }
-            = new ReactiveProperty<BindableBase>();
-
         public WireFrameGenerationViewModel()
         {
         }
