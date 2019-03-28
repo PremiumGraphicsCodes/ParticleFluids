f2a44f522003bd48aed2c6a5df2e2077e16a3957
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-03-28 14:21:07 +0900

    add texture view.

---
+++ CGStudio/PGControl/Graphics/TextureViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using System.Windows.Media.Imaging;
+
+namespace PG.Control.Graphics
+{
+    public class TextureViewModel : BindableBase
+    {
+        public ReactiveProperty<BitmapImage> Image { get; }
+
+        public TextureViewModel()
+        {
+            var image = new BitmapImage();
+            Image = new ReactiveProperty<BitmapImage>();
+            Image.Value = new BitmapImage();
+        }
+    }
+}
