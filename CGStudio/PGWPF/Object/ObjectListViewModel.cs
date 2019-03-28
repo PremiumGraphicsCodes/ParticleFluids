c53b69bb13a71beae2718461a6e5cdc6b6b067ce
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-03-28 11:49:36 +0900

    refactoring.

--- CGStudio/PGWPF/Object/ObjectListViewModel.cs
+++ CGStudio/PGWPF/Object/ObjectListViewModel.cs
@@ -1,53 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 
 namespace PG.CGStudio
 {
-    public class TreeSource : INotifyPropertyChanged
+    public class TreeSource : BindableBase
     {
-        private bool _IsExpanded = true;
-        private string _Text = "";
-        private TreeSource _Parent = null;
-        private ObservableCollection<TreeSource> _Children = null;
+        public ReactiveProperty<string> Name { get; }
+        public ReactiveProperty<bool> IsVisible { get; }
 
-
-        public bool IsExpanded
+        public TreeSource()
         {
-            get { return _IsExpanded; }
-            set { _IsExpanded = value; OnPropertyChanged("IsExpanded"); }
-        }
-
-        public string Text
-        {
-            get { return _Text; }
-            set { _Text = value; OnPropertyChanged("Text"); }
-        }
-
-        public TreeSource Parent
-        {
-            get { return _Parent; }
-            set { _Parent = value; OnPropertyChanged("Parent"); }
-        }
-
-        public ObservableCollection<TreeSource> Children
-        {
-            get { return _Children; }
-            set { _Children = value; OnPropertyChanged("Childen"); }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged(string name)
-        {
-            if (null == this.PropertyChanged) return;
-            this.PropertyChanged(this, new PropertyChangedEventArgs(name));
-        }
-
-        public void Add(TreeSource child)
-        {
-            if (null == Children) Children = new ObservableCollection<TreeSource>();
-            child.Parent = this;
-            Children.Add(child);
+            Name = new ReactiveProperty<string>();
+            IsVisible = new ReactiveProperty<bool>();
         }
     }
 
@@ -58,16 +23,14 @@ namespace PG.CGStudio
         public ObjectListViewModel()
         {
             TreeRoot = new ObservableCollection<TreeSource>();
-            var item1 = new TreeSource() { Text = "Object1", IsExpanded = true };
-            var item11 = new TreeSource() { Text = "Object1-1", IsExpanded = true };
-            var item12 = new TreeSource() { Text = "Object1-2", IsExpanded = true };
-            var item2 = new TreeSource() { Text = "Object2", IsExpanded = false };
-            var item21 = new TreeSource() { Text = "Object2-1", IsExpanded = true };
+            var item1 = new TreeSource();
+            item1.Name.Value = "Object1";
+            item1.IsVisible.Value = true;
+            var item2 = new TreeSource();
+            item2.Name.Value = "Object2";
+            item2.IsVisible.Value = false;
             TreeRoot.Add(item1);
             TreeRoot.Add(item2);
-            item1.Add(item11);
-            item1.Add(item12);
-            item2.Add(item21);
         }
     }
 }
