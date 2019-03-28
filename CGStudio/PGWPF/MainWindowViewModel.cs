e454ec44944ed92d3b6de5b4838d6e6326ff6b79
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-03-28 12:23:55 +0900

    refactoring.

--- CGStudio/PGWPF/MainWindowViewModel.cs
+++ CGStudio/PGWPF/MainWindowViewModel.cs
@@ -6,12 +6,6 @@ namespace PG.CGStudio
 {
     public class MainWindowViewModel : BindableBase
     {
-        public DelegateCommand<string> PCGenerationNavigateCommand { get; }
-
-        public DelegateCommand<string> WFGenerationNavigateCommand { get; }
-
-        public DelegateCommand<string> PMGenerationNavigateCommand { get; }
-
         public DelegateCommand<string> NavigateCommand { get; }
 
         private IRegionManager regionManager;
@@ -19,29 +13,7 @@ namespace PG.CGStudio
         public MainWindowViewModel(IRegionManager regionManager)
         {
             this.regionManager = regionManager;
-            PCGenerationNavigateCommand = new DelegateCommand<string>(OnPCGenerationNavigate);
-            WFGenerationNavigateCommand = new DelegateCommand<string>(OnWFGenerationNavigate);
-            PMGenerationNavigateCommand = new DelegateCommand<string>(OnPMGenerationNaviate);
-
             NavigateCommand = new DelegateCommand<string>(OnNavigate);// (name => );
-        }
-
-        private void OnPCGenerationNavigate(string name)
-        {
-            regionManager.RequestNavigate("ContentRegion", "PCGeneration");
-            regionManager.RequestNavigate("PCGenerationRegion", name);
-        }
-
-        private void OnWFGenerationNavigate(string name)
-        {
-            regionManager.RequestNavigate("ContentRegion", "WFGeneration");
-            regionManager.RequestNavigate("WFGenerationRegion", name);
-        }
-
-        private void OnPMGenerationNaviate(string name)
-        {
-            regionManager.RequestNavigate("ContentRegion", "PMGeneration");
-            regionManager.RequestNavigate("PMGenerationRegion", name);
         }
 
         private void OnNavigate(string name)
