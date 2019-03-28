33cae531c0d54a5e959610e56816d7cb2329c188
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-03-28 15:41:22 +0900

    show image.

--- CGStudio/PGControl/Graphics/TextureViewModel.cs
+++ CGStudio/PGControl/Graphics/TextureViewModel.cs
@@ -1,18 +1,32 @@
 ﻿using Prism.Mvvm;
 using Reactive.Bindings;
+using System.Windows.Forms;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PG.Control.Graphics
 {
     public class TextureViewModel : BindableBase
     {
-        public ReactiveProperty<BitmapImage> Image { get; }
+        public ReactiveProperty<string> ImagePath { get; }
+
+        public ReactiveCommand FileSelectCommand { get; }
 
         public TextureViewModel()
         {
-            var image = new BitmapImage();
-            Image = new ReactiveProperty<BitmapImage>();
-            Image.Value = new BitmapImage();
+            ImagePath = new ReactiveProperty<string>();
+            FileSelectCommand = new ReactiveCommand();
+            FileSelectCommand.Subscribe(OnFileSelect);
+        }
+
+        private void OnFileSelect()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(DialogResult.OK == dialog.ShowDialog() )
+            {
+                ImagePath.Value = dialog.FileName;
+            }
         }
     }
 }
