e5b13f72e52ef5a41bd37c1eb9cbe45db245720a
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-06-10 09:58:16 +0900

    add ui ctrl.

--- CGStudio/CGStudio/UICtrl/ObjectPickUICtrl.cs
+++ CGStudio/CGStudio/UICtrl/ObjectPickUICtrl.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PG.Core.Math;
+﻿using PG.Core.Math;
 
 namespace PG.CGStudio.UICtrl
 {
