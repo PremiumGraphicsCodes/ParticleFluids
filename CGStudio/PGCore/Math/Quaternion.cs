f1cf05ba748935ca0de01b7b4c20c2aade031993
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-06-17 13:23:07 +0900

    add functions.

--- CGStudio/PGCore/Math/Quaternion.cs
+++ CGStudio/PGCore/Math/Quaternion.cs
@@ -7,12 +7,38 @@
         private double z;
         private double w;
 
+        public Quaternion()
+        {
+            this.x = 0.0;
+            this.y = 0.0;
+            this.z = 0.0;
+            this.w = 0.0;
+        }
+
         public Quaternion(double x, double y, double z, double w)
         {
             this.x = x;
             this.y = y;
             this.z = z;
             this.w = w;
+        }
+
+        public Quaternion(Vector3d axis, double angle)
+        {
+            var s = System.Math.Sin(angle);
+            w = System.Math.Cos(angle);
+            x = axis.X * s;
+            y = axis.Y * s;
+            z = axis.Z * s;
+        }
+
+        public bool IsSame(Quaternion rhs, double tolerance)
+        {
+            return
+                System.Math.Abs(this.x - rhs.x) < tolerance &&
+                System.Math.Abs(this.y - rhs.y) < tolerance &&
+                System.Math.Abs(this.z - rhs.z) < tolerance &&
+                System.Math.Abs(this.w - rhs.w) < tolerance;
         }
 
         public Matrix3d ToRotationMatrix()
@@ -89,6 +115,85 @@
                     w = v;
                     break;
             }
+        }
+
+        public double Norm
+        {
+            get { return System.Math.Sqrt( x*x + y*y + z*z + w*w ); }
+        }
+
+        public Quaternion Normalized()
+        {
+            var n = Norm;
+            return new Quaternion(x / n, y / n, z / n, w / n);
+        }
+
+        public double Dot(Quaternion rhs)
+        {
+            return x * rhs.x + y * rhs.y + z * rhs.z + w * rhs.w;
+        }
+
+        public static Quaternion operator-(Quaternion rhs)
+        {
+            return new Quaternion(-rhs.x, -rhs.y, -rhs.z, -rhs.w);
+        }
+
+        public static Quaternion operator+(Quaternion lhs, Quaternion rhs)
+        {
+            return new Quaternion(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z, lhs.w + rhs.w);
+        }
+
+        public static Quaternion operator-(Quaternion lhs, Quaternion rhs)
+        {
+            return new Quaternion(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z, lhs.w - rhs.w);
+        }
+
+        public static Quaternion operator*(double lhs, Quaternion rhs)
+        {
+            return new Quaternion(lhs * rhs.x, lhs * rhs.y, lhs * rhs.z, lhs * rhs.w);
+        }
+
+        public Quaternion Slerp(Quaternion lhs, Quaternion rhs, double t)
+        {
+            // Only unit quaternions are valid rotations.
+            // Normalize to avoid undefined behavior.
+            var v0 = lhs.Normalized();
+            var v1 = rhs.Normalized();
+
+            // Compute the cosine of the angle between the two vectors.
+            var dot = v0.Dot(v1);
+
+            // If the dot product is negative, slerp won't take
+            // the shorter path. Note that v1 and -v1 are equivalent when
+            // the negation is applied to all four components. Fix by 
+            // reversing one quaternion.
+            if (dot < 0.0f)
+            {
+                v1 = -v1;
+                dot = -dot;
+            }
+
+            const double DOT_THRESHOLD = 0.9995;
+            if (dot > DOT_THRESHOLD)
+            {
+                // If the inputs are too close for comfort, linearly interpolate
+                // and normalize the result.
+
+                Quaternion result = v0 + t * (v1 - v0);
+                result = result.Normalized();
+                return result;
+            }
+
+            // Since dot is in range [0, DOT_THRESHOLD], acos is safe
+            double theta_0 = System.Math.Acos(dot);        // theta_0 = angle between input vectors
+            double theta = theta_0 * t;          // theta = angle between v0 and result
+            double sin_theta = System.Math.Sin(theta);     // compute this value only once
+            double sin_theta_0 = System.Math.Sin(theta_0); // compute this value only once
+
+            double s0 = System.Math.Cos(theta) - dot * sin_theta / sin_theta_0;  // == sin(theta_0 - theta) / sin(theta_0)
+            double s1 = sin_theta / sin_theta_0;
+
+            return (s0 * v0) + (s1 * v1);
         }
     }
 }
---
+++ CGStudio/PGCoreTest/Math/QuaternionTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PG.Core.Math;
+
+namespace PGCoreTest.Math
+{
+    [TestClass]
+    public class QuaternionTest
+    {
+        [TestMethod]
+        public void TestFromRotationMatrix()
+        {
+            var axis = new Vector3d(1,0,0);
+            var angle = System.Math.PI / 0.5;
+            var expected = new Quaternion(axis, angle);
+            var m = Matrix3d.RotationX(angle);
+
+            var q = new Quaternion();
+            q.FromRotationMatrix(m);
+            Assert.IsTrue(expected.IsSame(q, 1.0e-6));
+        }
+
+        /*
+        [TestMethod]
+        public void TestToRotationMatrix()
+        {
+            var q = new Quaternion(1.0, 0.0, 0.0, 0.0);
+            var m = q.ToRotationMatrix();
+
+            var expected = Matrix3d.RotationX(0.5 * System.Math.PI);
+        }
+        */
+    }
+}
