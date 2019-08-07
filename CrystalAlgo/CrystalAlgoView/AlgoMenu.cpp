4113d5f92b8ecf184de63f4d038b279e89be7141
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-08-07 13:34:38 +0900

    refactoring.

---
+++ CrystalAlgo/CrystalAlgoView/AlgoMenu.cpp
@@ -0,0 +1,49 @@
+#include "AlgoMenu.h"
+
+#include "IntersectionView.h"
+#include "BooleanView.h"
+#include "SpaceHashView.h"
+#include "MarchingCubesView.h"
+#include "VolumeView.h"
+#include "VolumeConvertView.h"
+#include "OctreeView.h"
+#include "SpaceHashView.h"
+
+#include "../../Crystal/AppBase/imgui.h"
+
+using namespace Crystal::Scene;
+using namespace Crystal::UI;
+
+AlgoMenu::AlgoMenu(const std::string& name, RootScene* model, Canvas* canvas, ControlPanel* control) :
+	IMenu(name, model, canvas),
+	control(control)
+{
+}
+
+void AlgoMenu::show()
+{
+	if (ImGui::BeginMenu("Algo")) {
+		if (ImGui::MenuItem("Boolean")) {
+			control->setWindow(new BooleanView(getModel(), getCanvas()));
+		}
+		if (ImGui::MenuItem("SpaceHash")) {
+			control->setWindow(new SpaceHashView(getModel(), getCanvas()));
+		}
+		if (ImGui::MenuItem("Octree")) {
+			control->setWindow(new OctreeView(getModel(), getCanvas()));
+		}
+		if (ImGui::MenuItem("Intersection")) {
+			control->setWindow(new IntersectionView(getModel(), getCanvas()));
+		}
+		if (ImGui::MenuItem("Volume")) {
+			control->setWindow(new VolumeView(getModel(), getCanvas()));
+		}
+		if (ImGui::MenuItem("VolumeConvert")) {
+			control->setWindow(new VolumeConvertView(getModel(), getCanvas()));
+		}
+		if (ImGui::MenuItem("MarchingCubes")) {
+			control->setWindow(new MarchingCubesView(getModel(), getCanvas()));
+		}
+		ImGui::EndMenu();
+	}
+}
--- CrystalAlgo/CrystalAlgoView/AlgoMenu.h
+++ CrystalAlgo/CrystalAlgoView/AlgoMenu.h
@@ -3,56 +3,19 @@
 #include "../../Crystal/AppBase/IMenu.h"
 #include "../../Crystal/AppBase/ControlPanel.h"
 
-#include "IntersectionView.h"
-#include "BooleanView.h"
-#include "SpaceHashView.h"
-#include "MarchingCubesView.h"
-#include "VolumeView.h"
-#include "VolumeConvertView.h"
-#include "OctreeView.h"
-#include "SpaceHashView.h"
-
-#include "../../Crystal/AppBase/imgui.h"
-
 namespace Crystal {
 	namespace UI {
-		class AlgoMenu : public IMenu
-		{
-		public:
-			AlgoMenu(const std::string& name, Scene::RootScene* model, Canvas* canvas, ControlPanel* control) :
-				IMenu(name, model, canvas),
-				control(control)
-			{
-				/*
-				add(new SpaceHashView(model, canvas));
-				add(new MarchingCubesView(model, canvas));
-				add(new VolumeButton(model, canvas));
-				add(new VolumeConvertButton(model, canvas));
-				add(new SpaceHashView(model, canvas));
-				*/
-			}
 
-			void show() override
-			{
-				if (ImGui::BeginMenu("Algo")) {
-					if (ImGui::MenuItem("Boolean")) {
-						control->setWindow(new BooleanView(getModel(), getCanvas()));
-					}
-					if (ImGui::MenuItem("SpaceHash")) {
-						control->setWindow(new SpaceHashView(getModel(), getCanvas()));
-					}
-					if (ImGui::MenuItem("Intersection")) {
-						control->setWindow(new IntersectionView(getModel(), getCanvas()));
-					}
-					if (ImGui::MenuItem("VolumeConvert")) {
-						control->setWindow(new VolumeConvertView(getModel(), getCanvas()));
-					}
-					ImGui::EndMenu();
-				}
-			}
+class AlgoMenu : public IMenu
+{
+public:
+	AlgoMenu(const std::string& name, Scene::RootScene* model, Canvas* canvas, ControlPanel* control);
 
-		private:
-			ControlPanel* control;
-		};
+	void show() override;
+
+private:
+	ControlPanel* control;
+};
+
 	}
 }
