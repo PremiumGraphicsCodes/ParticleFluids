a240d91ea4c43ec1c927df200251ee67d24e3a0c
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-10-03 14:59:16 +0900

    remove unused file.

--- CGStudio/PGCLI/WorldAdapter.cpp
+++ CGStudio/PGCLI/WorldAdapter.cpp
@@ -1,17 +1,9 @@
 #include "stdafx.h"
-#include <msclr/marshal_cppstd.h>
 
 #include "WorldAdapter.h"
-#include "SceneAdapter.h"
 
 #include "../../Crystal/Scene/World.h"
 //#include "../../Crystal/UI/ViewModel.h"
-
-#include "../../Crystal/IO/FileImporter.h"
-#include "../../Crystal/IO/FileExporter.h"
-
-#include "Converter.h"
-
 
 using namespace PG::CLI;
 
