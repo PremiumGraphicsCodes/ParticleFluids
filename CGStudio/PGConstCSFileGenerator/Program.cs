5f1926af87935d29844c9c07d276621fa3fe72ee
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-10-07 13:07:06 +0900

    add const file generator.

---
+++ CGStudio/PGConstCSFileGenerator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace PGConstCSFileGenerator
+{
+    class PublicConstant
+    {
+        public string Key { get; }
+        public string Value { get; }
+        public string ToCSSource()
+        {
+            return "public const string " + Key + " = " + Value;
+        }
+    }
+
+    class PublicConstants
+    {
+        public string Name { get; private set; }
+
+        public List<PublicConstant> Constants { get; private set; }
+            = new List<PublicConstant>();
+
+        public void ParseCPPFile(string filePath)
+        {
+            this.Name = Path.GetFileNameWithoutExtension(filePath);
+
+        }
+
+        public string ToCSSource(string filePath)
+        {
+            return "";
+        }
+    }
+
+    class CPPLine
+    {
+        CPPLine()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
