7c4a6ae4f74e0b6493311f74af6e730871c3445f
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-10-07 15:11:34 +0900

    implement label generator.

---
+++ Crystal/Command/Public/CameraLabels.h
@@ -0,0 +1,8 @@
+#define PublicLabel constexpr auto
+
+namespace {
+	PublicLabel CameraFitCommandLabel = "CameraFit";
+	PublicLabel CameraXYCommandLabel = "CameraXY";
+	PublicLabel CameraYZCommandLabel = "CameraYZ";
+	PublicLabel CameraZXCommandLabel = "CameraZX";
+}
