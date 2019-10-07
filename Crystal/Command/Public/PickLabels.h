5842a22137696d385fcf05b885675cc03d27a6ac
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-10-07 15:30:15 +0900

    add labels.

---
+++ Crystal/Command/Public/PickLabels.h
@@ -0,0 +1,8 @@
+#include "PublicLabel.h"
+
+namespace {
+	PublicLabel PickCommandLabel = "Pick";
+	PublicLabel PositionLabel = "Position";
+	PublicLabel ParentIdLabel = "ParentId";
+	PublicLabel ChildIdLabel = "ChildId";
+}
---
+++ Crystal/Command/Public/PublicLabel.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define PublicLabel constexpr auto
