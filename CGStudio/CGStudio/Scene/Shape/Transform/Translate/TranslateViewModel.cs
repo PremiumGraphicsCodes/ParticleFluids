9334808ed14a14345dd8e98ae16cb4ab0ff52687
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-10-07 11:48:18 +0900

    implement translate.

--- CGStudio/CGStudio/Scene/Shape/Transform/Translate/TranslateViewModel.cs
+++ CGStudio/CGStudio/Scene/Shape/Transform/Translate/TranslateViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using PG.CGStudio.Object.Select;
+using PG.CGStudio.UICtrl;
 using PG.Control.Math;
 using Prism.Regions;
+using Reactive.Bindings;
+using System.Collections.Generic;
 
 namespace PG.CGStudio.Scene.Shape.Transform
 {
@@ -11,6 +14,17 @@ namespace PG.CGStudio.Scene.Shape.Transform
 
         public Vector3dViewModel VectorViewModel { get; }
             = new Vector3dViewModel();
+
+        public ReactiveCommand TranslateCommand { get; }
+            = new ReactiveCommand();
+
+        public ReactiveCommand OkCommand { get; }
+            = new ReactiveCommand();
+
+        public TranslateViewModel()
+        {
+            this.TranslateCommand.Subscribe(OnTranslate);
+        }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
         {
@@ -23,6 +37,16 @@ namespace PG.CGStudio.Scene.Shape.Transform
 
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
+        }
+
+        private void OnTranslate()
+        {
+            var ids = new List<int>
+            {
+                ShapeSelectViewModel.Id.Value
+            };
+            var moveCtrl = new TranslateUICtrl(ids);
+            Canvas3d.Instance.UICtrl = moveCtrl;
         }
     }
 }
