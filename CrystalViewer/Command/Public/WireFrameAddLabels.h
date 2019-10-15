f8941954250554786947738fdaaac11055aba4ea
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-10-15 10:02:01 +0900

    add label.

---
+++ CrystalViewer/Command/Public/WireFrameAddLabels.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "PublicLabel.h"
+
+namespace {
+	PublicLabel ParticleSystemAddLabel = "ParticleSystemAdd";
+	PublicLabel PositionsLabel = "Positions";
+	PublicLabel PointSizeLabel = "PointSize";
+	PublicLabel ColorLabel = "Color";
+	PublicLabel NameLabel = "Name";
+	PublicLabel IsItemLabel = "IsItem";
+}
