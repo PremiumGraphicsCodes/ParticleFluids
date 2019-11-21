86b9f1d9c82f70aac68c48acec869b84cfa3f8e5
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-11-21 10:42:07 +0900

    add command.

---
+++ CrystalViewer/Command/TextureCreateCommand.cpp
@@ -0,0 +1,43 @@
+#include "TextureCreateCommand.h"
+
+#include "../../Crystal/IO/ImageFileReader.h"
+
+//#include "Public/MaterialCreateLabels.h"
+
+using namespace Crystal::Math;
+using namespace Crystal::Graphics;
+using namespace Crystal::Scene;
+using namespace Crystal::IO;
+using namespace Crystal::Command;
+
+std::string TextureCreateCommand::getName()
+{
+	return "TextureCreate"; //MaterialCreateLabels::CommandNameLabel;
+}
+
+TextureCreateCommand::Args::Args() :
+	filePath("FilePath", std::string("")),
+	name("Name", std::string(""))
+{
+	add(&filePath);
+}
+
+TextureCreateCommand::Results::Results() :
+	newId("NewId", -1) //MaterialCreateLabels::NewIdLabel, -1)
+{
+	add(&newId);
+}
+
+void TextureCreateCommand::execute(World* world)
+{
+	ImageFileReader reader;
+	const auto isOk = reader.read(args.filePath.getValue());
+	if (!isOk) {
+		return;
+	}
+	/*
+	auto scene = world->getObjectFactory()->create(m, args.name.getValue());
+	world->getObjects()->addScene(scene);
+	results.newId.setValue(scene->getId());
+	*/
+}
---
+++ CrystalViewer/Command/TextureCreateCommand.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "ICommand.h"
+
+namespace Crystal {
+	namespace Command {
+
+class TextureCreateCommand : public ICommand
+{
+public:
+	struct Args : IArgs
+	{
+		Args();
+
+		Arg<std::string> filePath;
+		Arg<std::string> name;
+	};
+
+	struct Results : IResults
+	{
+		Results();
+
+		Result<int> newId;
+	};
+
+	TextureCreateCommand() :
+		ICommand(&args, &results)
+	{}
+
+	static std::string getName();
+
+	void execute(Scene::World* world) override;
+
+private:
+	Args args;
+	Results results;
+};
+
+	}
+}
