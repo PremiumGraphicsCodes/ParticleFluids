8eb45b0cd1718b6cbe780b7b48fb3a97e8c6cb17
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-11-25 09:23:07 +0900

    add command.

---
+++ CrystalViewer/Command/ShapeSelectCommand.cpp
@@ -0,0 +1,40 @@
+#include "ShapeSelectCommand.h"
+#include "../../Crystal/Scene/IShapeScene.h"
+#include "../../Crystal/Shape/WireFrameBuilder.h"
+
+using namespace Crystal::Math;
+using namespace Crystal::Command;
+using namespace Crystal::Scene;
+
+std::string ShapeSelectCommand::getName()
+{
+	return "ShapeSelectCommand";
+}
+
+ShapeSelectCommand::Args::Args() :
+	shapeId("ShapeId", -1)
+{
+}
+
+ShapeSelectCommand::Results::Results() :
+	boundingBoxItemId("BoundingBoxItemId", -1)
+{
+}
+
+void ShapeSelectCommand::execute(World* scene)
+{
+	auto shape = scene->getObjects()->findSceneById<IShapeScene*>(args.shapeId.getValue());
+	if (shape == nullptr) {
+		return;
+	}
+	auto isSelected = shape->isSelected();
+	if (isSelected) {
+		Box3d bb;
+		shape->getBoundingBox(bb);
+		WireFrameBuilder builder;
+		builder.build(bb);
+//		scene->getItemFactory()->createWireFrameScene()
+
+		shape->setSelected(true);
+	}
+}
---
+++ CrystalViewer/Command/ShapeSelectCommand.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "ICommand.h"
+#include "../../Crystal/Math/Vector3d.h"
+#include "../../Crystal/Graphics/ColorRGBA.h"
+#include <vector>
+
+namespace Crystal {
+	namespace Command {
+
+class ShapeSelectCommand : public ICommand
+{
+public:
+	ShapeSelectCommand() :
+		ICommand(&args, &results)
+	{}
+
+	struct Args : IArgs
+	{
+		Args();
+
+		Arg< int > shapeId;
+	};
+
+	struct Results : IResults
+	{
+		Results();
+
+		Result<int> boundingBoxItemId;
+	};
+
+	static std::string getName();
+
+	void execute(Scene::World* scene) override;
+
+private:
+	Args args;
+	Results results;
+};
+	}
+}
