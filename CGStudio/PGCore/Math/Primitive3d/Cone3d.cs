71af2e626cdfd505776d9f239257602d12b262b2
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-12-09 10:50:29 +0900

    add Cone3d.

---
+++ CGStudio/PGCore/Math/Primitive3d/Cone3d.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PG.Core.Math
+{
+    public class Cone3d : ISurface3d, IVolume3d
+    {
+        public Cone3d()
+        {
+            Radius = 1.0;
+            Height = 1.0;
+            Bottom = new Vector3d(0, 0, 0);
+        }
+
+        public Cone3d(double radius, double height, Vector3d bottom)
+        {
+            Radius = radius;
+            Height = height;
+            Bottom = bottom;
+        }
+
+        public double Radius { get; }
+
+        public double Height { get; }
+
+        public Vector3d Bottom { get; }
+
+        public Vector3d GetPosition(double u, double v)
+        {
+            return GetPosition(u, v, 1.0);
+        }
+
+        public Vector3d GetPosition(double u, double v, double w)
+        {
+            var uu = u * 2.0 * System.Math.PI;
+            var r = w * Radius * (1.0 - v);
+            var x = r * System.Math.Cos(uu);
+            var y = r * System.Math.Sin(uu);
+            var z = v * Height;
+            return Bottom + new Vector3d(x, y, z);
+        }
+
+        public bool IsInside(Vector3d position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CGStudio/PGCore/Shape/Glyph/ArrowGlyph3d.cs
@@ -0,0 +1,22 @@
+﻿using PG.Core.Math;
+
+namespace PG.Core.Shape.Glyph
+{
+    public class ArrowGlyph3d : IGlyph3d
+    {
+        Cylinder3d cylider;
+        Cone3d cone;
+
+        public ArrowGlyph3d(Vector3d origin, Vector3d direction)
+        {
+            this.cylider = new Cylinder3d();
+            this.cone = new Cone3d();
+        }
+
+        public void Build()
+        {
+        }
+
+        //public WireFrame ToWireFrame();
+    }
+}
