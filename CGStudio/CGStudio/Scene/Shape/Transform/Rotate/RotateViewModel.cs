b087b376cc7f363adf4b7d164141f2b36223770b
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2019-12-27 14:51:15 +0900

    rotate.

--- CGStudio/CGStudio/Scene/Shape/Transform/Rotate/RotateViewModel.cs
+++ CGStudio/CGStudio/Scene/Shape/Transform/Rotate/RotateViewModel.cs
@@ -22,10 +22,14 @@ namespace PG.CGStudio.Scene.Shape.Transform
         public ReactiveCommand OkCommand { get; }
             = new ReactiveCommand();
 
+        public ReactiveCommand CancelCommand { get; }
+            = new ReactiveCommand();
+
         public RotateViewModel()
         {
             this.RotateCommand.Subscribe(OnRotate);
             this.OkCommand.Subscribe(OnOk);
+            this.CancelCommand.Subscribe(OnCancel);
         }
 
         private void OnRotate()
@@ -38,17 +42,8 @@ namespace PG.CGStudio.Scene.Shape.Transform
 
         private void OnChanged(double x)
         {
-            var rx = AngleViewModel.X.Value / 180.0 * System.Math.PI;
-            var ry = AngleViewModel.Y.Value / 180.0 * System.Math.PI;
-            var rz = AngleViewModel.Z.Value / 180.0 * System.Math.PI;
-
-            var matrix = Matrix3d.RotationZ(rz) * Matrix3d.RotationY(ry) * Matrix3d.RotationX(rx);
-
             var canvas = Canvas3d.Instance;
-            var command = new PG.CLI.Command(TransformLabels.RotateCommandLabel);
-            command.SetArg(TransformLabels.IdLabel, ShapeSelectViewModel.Id.Value);
-            command.SetArg(TransformLabels.MatrixLabel, matrix);
-            command.Execute(MainModel.Instance.World.Adapter);
+            MainModel.Instance.World.Scenes.SetMatrix(ShapeSelectViewModel.Id.Value, ToMatrix());
 
             canvas.Update(MainModel.Instance.World);
             canvas.Render();
@@ -56,8 +51,26 @@ namespace PG.CGStudio.Scene.Shape.Transform
 
         private void OnOk()
         {
+            MainModel.Instance.World.Scenes.Transform(ShapeSelectViewModel.Id.Value, ToMatrix());
 
+            OnCancel();
         }
+
+        private void OnCancel()
+        {
+            AngleViewModel.Value = new Vector3d(0, 0, 0);
+        }
+
+        private Matrix4d ToMatrix()
+        {
+            var rx = AngleViewModel.X.Value / 180.0 * System.Math.PI;
+            var ry = AngleViewModel.Y.Value / 180.0 * System.Math.PI;
+            var rz = AngleViewModel.Z.Value / 180.0 * System.Math.PI;
+
+            var matrix = Matrix3d.RotationZ(rz) * Matrix3d.RotationY(ry) * Matrix3d.RotationX(rx);
+            return new Matrix4d(matrix);
+        }
+
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
         {
