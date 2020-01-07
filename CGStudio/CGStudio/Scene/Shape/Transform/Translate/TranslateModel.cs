0f78b6bdc23c05d5e69d9c2b83bfb6eb769198c1
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2020-01-07 13:57:11 +0900

    divide file.

---
+++ CGStudio/CGStudio/Scene/Shape/Transform/Translate/TranslateModel.cs
@@ -0,0 +1,27 @@
+﻿using PG.Control.Math;
+using PG.Core.Math;
+using Reactive.Bindings;
+
+namespace PG.CGStudio.Scene.Shape.Transform
+{
+    public class TranslateModel
+    {
+        public ReactiveProperty<int> Id { get; }
+            = new ReactiveProperty<int>();
+
+        public Vector3dViewModel Translate { get; }
+            = new Vector3dViewModel();
+
+        public Matrix4d ToMatrix()
+        {
+            var t = Translate.Value;
+            return new Matrix4d
+                (
+                1.0, 0.0, 0.0, 0.0,
+                0.0, 1.0, 0.0, 0.0,
+                0.0, 0.0, 1.0, 0.0,
+                t.X, t.Y, t.Z, 1.0
+                );
+        }
+    }
+}
--- CGStudio/CGStudio/Scene/Shape/Transform/Translate/TranslateViewModel.cs
+++ CGStudio/CGStudio/Scene/Shape/Transform/Translate/TranslateViewModel.cs
@@ -8,27 +8,6 @@ using System;
 
 namespace PG.CGStudio.Scene.Shape.Transform
 {
-    public class TranslateModel
-    {
-        public ReactiveProperty<int> Id { get; }
-            = new ReactiveProperty<int>();
-
-        public Vector3dViewModel Translate { get; }
-            = new Vector3dViewModel();
-
-        public Matrix4d ToMatrix()
-        {
-            var t = Translate.Value;
-            return new Matrix4d
-                (
-                1.0, 0.0, 0.0, 0.0,
-                0.0, 1.0, 0.0, 0.0,
-                0.0, 0.0, 1.0, 0.0,
-                t.X, t.Y, t.Z, 1.0
-                );
-        }
-    }
-
     public class TranslateViewModel : INavigationAware
     {
         public ReactiveProperty<int> ShapeId { get { return model.Id; } }
