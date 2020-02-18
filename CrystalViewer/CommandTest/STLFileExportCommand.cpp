efac1380d27dac5ece7ca1b09891ee3b6733763c
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2020-02-18 14:26:25 +0900

    fix STL file export.

--- CrystalViewer/CommandTest/STLFileExportCommand.cpp
+++ CrystalViewer/CommandTest/STLFileExportCommand.cpp
@@ -2,9 +2,9 @@
 
 #include "../Command/Command.h"
 
+#include "../../Crystal/Scene/PolygonMeshScene.h"
 #include "../../Crystal/Shape/PolygonMeshBuilder.h"
 
-#include "../Command/Public/PolygonMeshCreateLabels.h"
 #include "../Command/Public/FileExportLabels.h"
 
 using namespace Crystal::Math;
@@ -18,19 +18,12 @@ TEST(STLFileExportCommand, TestExecute)
 
 	PolygonMeshBuilder builder;
 	builder.add(Box3d());
+	auto scene = world.getSceneFactory()->createPolygonMeshScene(builder.build(), "");
+	world.getObjects()->addScene(scene);
 
-	Crystal::Command::Command command(PolygonMeshCreateLabels::CommandNameLabel);
-	command.setArg(PolygonMeshCreateLabels::PositionsLabel, builder.getPositions());
-	command.setArg(PolygonMeshCreateLabels::VerticesLabel, builder.getVertices());
-	command.setArg(PolygonMeshCreateLabels::NormalsLabel, builder.getNormals());
-	command.setArg(PolygonMeshCreateLabels::TexCoordsLabel, builder.getTexCoords());
-	command.setArg(PolygonMeshCreateLabels::FacesLabel, builder.getFaces());
-	EXPECT_TRUE(command.execute(&world));
-
-	const auto newId = std::any_cast<int>(command.getResult(PolygonMeshCreateLabels::NewIdLabel));
-	const std::vector<int> newIds = { newId };
+	const std::vector<int> newIds = { scene->getId() };
 	const std::string filePath = "./STLFileExportTest.stl";
-	command.create(FileExportLabels::STLFileExportCommandLabel);
+	Command command(FileExportLabels::STLFileExportCommandLabel);
 	command.setArg(FileExportLabels::IdsLabel, newIds);
 	command.setArg(FileExportLabels::FilePathLabel, filePath);
 	EXPECT_TRUE(command.execute(&world));
