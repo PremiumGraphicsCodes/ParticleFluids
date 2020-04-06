e7ed5650d78d1c8da9133b6883fd59a81f5f4a46
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2020-04-06 13:07:04 +0900

    implement controller.

--- Crystal/Scene/CameraSceneController.cpp
+++ Crystal/Scene/CameraSceneController.cpp
@@ -1,1 +1,20 @@
 #include "CameraSceneController.h"
+
+using namespace Crystal::Shader;
+using namespace Crystal::Scene;
+
+CameraSceneController::CameraSceneController(CameraScene* model) :
+	model(model),
+	view(nullptr)
+{}
+
+void CameraSceneController::createView(SceneShader* sceneShader, GLObjectFactory& factory)
+{
+
+}
+
+void CameraSceneController::updateView()
+{
+
+}
+
--- Crystal/Scene/CameraSceneController.h
+++ Crystal/Scene/CameraSceneController.h
@@ -1,22 +1,27 @@
 #pragma once
 
 namespace Crystal {
+	namespace Shader {
+		class GLObjectFactory;
+	}
 	namespace Scene {
 		class CameraScene;
 		class CameraShaderScene;
+		class SceneShader;
 
 class CameraSceneController
 {
 public:
 	explicit CameraSceneController(CameraScene* model);
 
-	//void createView(SceneShader* sceneShader, Shader::GLObjectFactory& factory);
+	void createView(SceneShader* sceneShader, Shader::GLObjectFactory& factory);
 
 	void updateView();
 
-	//PointShaderScene* getView() { return view; }
+	CameraShaderScene* getView() { return view; }
 
 private:
+	CameraScene* model;
 	CameraShaderScene* view;
 };
 
