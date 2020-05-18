4306d510b698f9bc9119f39d4ff8596fc5ec606a
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2020-05-18 12:50:17 +0900

    refactor.

--- CGStudio/PGControl/OpenGL/HwndHostPresenter3d.cs
+++ CGStudio/PGControl/OpenGL/HwndHostPresenter3d.cs
@@ -8,6 +8,7 @@ using PG.Core.Math;
 using PG.Core;
 using PG.CGStudio.UICtrl;
 using System.Windows.Input;
+using PG.Control.OpenGL;
 
 namespace PG.CGStudio
 {
@@ -20,90 +21,6 @@ namespace PG.CGStudio
         protected override void OnPaintBackground(System.Windows.Forms.PaintEventArgs pevent)
         {
             ;
-        }
-    }
-
-    public class Canvas3d : IUICtrl
-    {
-        private UICtrl.IUICtrl ctrl;
-        public UICtrl.IUICtrl UICtrl
-        {
-            get { return ctrl; }
-            set { this.ctrl = value; }
-        }
-
-        private PG.CLI.Renderer renderer;
-        public PG.CLI.Renderer Renderer { get { return renderer; } }
-
-        public Canvas3d()
-        {
-            this.ctrl = new UICtrl.CameraUICtrl();
-        }
-
-        public void CreateRenderer(System.IntPtr handle, World world)
-        {
-            renderer = new PG.CLI.Renderer(handle, World.Instance.Adapter);
-            this.renderer.Build(world.Adapter);
-        }
-
-        public void OnLeftButtonDown(Vector2d position)
-        {
-            this.ctrl.OnLeftButtonDown(position);
-        }
-
-        public void OnLeftButtonDragging(Vector2d position)
-        {
-            this.ctrl.OnLeftButtonDragging(position);
-        }
-
-        public void OnLeftButtonUp(Vector2d position)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnRightButtonDown(Vector2d position)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnRightButtonDragging(Vector2d position)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnRightButtonUp(Vector2d position)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnMiddleButtonDown(Vector2d position)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnMiddleButtonDragging(Vector2d position)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnMiddleButtonUp(Vector2d position)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnWheel(double dx)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnKeyDown(Key key)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnKeyUp(Key key)
-        {
-            this.UICtrl.OnKeyUp(key);
         }
     }
 
