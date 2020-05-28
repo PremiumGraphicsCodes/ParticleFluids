9ab3b819d76661aebe44932ecdb2f32fac857bce
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2020-05-28 16:07:43 +0900

    add view.

---
+++ CGStudio/PGControl/Math/Torus3dViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.Control.Math
+{
+    public class Torus3dViewModel
+    {
+        public Vector3dViewModel BottomViewModel { get; }
+            = new Vector3dViewModel();
+
+        public ReactiveProperty<double> BigRadius { get; }
+            = new ReactiveProperty<double>(10.0);
+
+        public ReactiveProperty<double> SmallRadius { get; }
+            = new ReactiveProperty<double>(1.0);
+
+        public Torus3dViewModel()
+        { }
+    }
+}
