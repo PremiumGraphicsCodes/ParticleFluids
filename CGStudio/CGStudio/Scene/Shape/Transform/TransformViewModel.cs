d7b652a5cec1cc83af4d317781da761540cfa2b2
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2020-06-01 10:51:14 +0900

    implement Transform.

--- CGStudio/CGStudio/Scene/Shape/Transform/TransformViewModel.cs
+++ CGStudio/CGStudio/Scene/Shape/Transform/TransformViewModel.cs
@@ -1,44 +1,17 @@
-﻿using PG.CGStudio.Object.Select;
-using PG.CGStudio.Scene.Shape.Transform;
-using PG.CGStudio.Scene.Shape.Transform.Scale;
+﻿using PG.Control.Math;
 using PG.Control.OpenGL;
 using PG.Scene;
 using Prism.Mvvm;
-using Prism.Regions;
 
 namespace PG.CGStudio.Object.Transform
 {
-    public class TransformViewModel : BindableBase, INavigationAware
+    public class TransformViewModel : BindableBase
     {
-        public ShapeSelectViewModel SelectViewModel { get; }
-
-        public TranslateViewModel TranslateViewModel { get; }
-
-        public ScaleViewModel ScaleViewModel { get; }
-
-        public RotateViewModel RotateViewModel { get; }
+        public Matrix4dViewModel MatrixViewModel { get; }
+            = new Matrix4dViewModel();
         
         public TransformViewModel(SceneList world, Canvas3d canvas)
         {
-            this.SelectViewModel = new ShapeSelectViewModel(world, canvas);
-            this.TranslateViewModel = new TranslateViewModel(world, canvas);
-            this.ScaleViewModel = new ScaleViewModel(world, canvas);
-            this.RotateViewModel = new RotateViewModel(world, canvas);
-        }
-
-        public void OnNavigatedTo(NavigationContext navigationContext)
-        {
-
-        }
-
-        public bool IsNavigationTarget(NavigationContext navigationContext)
-        {
-            return true;
-        }
-
-        public void OnNavigatedFrom(NavigationContext navigationContext)
-        {
-
         }
     }
 }
---
+++ CGStudio/PGControl/UI/SceneSelectView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace PG.Control.UI
+{
+    public partial class SceneSelectView : UserControl
+    {
+        public SceneSelectView()
+        {
+            InitializeComponent();
+        }
+    }
+}
