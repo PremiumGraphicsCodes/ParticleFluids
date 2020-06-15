75e4dcf66e3aaffe49c213bbd7995f959b4af9da
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2020-06-15 10:45:45 +0900

    add PBSPH.

---
+++ CrystalAlgo/CrystalPhysics/PBSPHParticle.h
@@ -0,0 +1,115 @@
+#pragma once
+
+#include <list>
+#include <vector>
+#include <memory>
+
+#include "../../Crystal/Math/Vector3d.h"
+#include "SPHKernel.h"
+//#include "ISPHParticle.h"
+#include "SPHConstant.h"
+
+namespace Crystal {
+	namespace Physics {
+
+class PBSPHParticle //: public ISPHParticle
+{
+public:
+	PBSPHParticle(const Math::Vector3df& center, float radius, SPHConstant* constant);
+
+	void setKernel(SPHKernel* kernel) { this->kernel = kernel; }
+
+	virtual ~PBSPHParticle() {};
+
+	float getDensityRatio() const;
+
+	float getMass() const;
+
+	float getVolume() const;
+
+	float getRestVolume() const;
+
+	void addForce(const Math::Vector3df& force) { this->force += force; }
+
+	void setForce(const Math::Vector3df& force) { this->force = force; }
+
+	Math::Vector3df getForce() const { return force; }
+
+	void setDefaultDensity() { this->density = constant->getDensity(); }
+
+	float getDensity() const { return density; }
+
+	void addDensity(const float density) { this->density += density; }
+
+	void init();
+
+	Math::Vector3df getAccelaration() { return force / density; }
+
+	Math::Vector3df getVelocity() const { return velocity; }
+
+	void setVelocity(const Math::Vector3df& velocity) { this->velocity = velocity; }
+
+	void addVelocity(const Math::Vector3df& velocity) {
+		this->velocity += velocity;
+		//this->velocity *= 0.999;
+	}
+
+	void forwardTime(const float timeStep);
+
+	void addExternalForce(const Math::Vector3df& force);
+
+	void addSelfDensity();
+
+	void addDensity(const PBSPHParticle& rhs);
+
+	void addDensity(const float distance, const float mass);
+
+	void predictPosition_(const float dt);
+
+	void updatePredictPosition();
+
+	void updateVelocity(const float dt);
+
+	void updatePosition();
+
+	void addPositionCorrection(const Math::Vector3df& pc);
+
+	void solveViscosity(const float distance);
+
+	float getEffectLength() const { return constant->getEffectLength(); }
+
+	Math::Vector3df getPredictPosition() const { return predictPosition; }
+
+	Math::Vector3df getPosition() const { return position; }
+
+	void calculatePressure(const PBSPHParticle& rhs);
+
+	void calculatePressure(const Math::Vector3df& v);
+
+	void calculateViscosity(const PBSPHParticle& rhs);
+
+	float getConstraint() const;
+
+	Math::Vector3df dx;
+	Math::Vector3df xvisc;
+
+	void setDensity(const float d) { this->density = d; }
+
+
+private:
+	Math::Vector3df getDiff(const PBSPHParticle& rhs) const;
+
+	Math::Vector3df predictPosition;
+	Math::Vector3df force;
+	Math::Vector3df velocity;
+	Math::Vector3df normal;
+
+	Math::Vector3df position;
+	float density;
+
+	SPHConstant* constant;
+	SPHKernel* kernel;
+};
+
+	}
+}
---
+++ CrystalAlgo/CrystalPhysics/SPHKernel.h
@@ -0,0 +1,52 @@
+#include "../../Crystal/Math/Vector3d.h"
+#include "../../Crystal/Math/Matrix3d.h"
+
+namespace Crystal {
+	namespace Physics {
+
+class SPHKernel
+{
+public:
+	SPHKernel() = default;
+
+	SPHKernel(const float effectLength);
+
+	float getPoly6Kernel(const float distance);
+
+	float getPoly6Kernel2(const float distanceSquared);
+
+	float getPoly6Kernel(const float distance, const float effectLength);
+
+	Math::Vector3df getPoly6KernelGradient(const Math::Vector3df& distanceVector, const float effectLength);
+
+	float getPoly6KernelGradientCoe(const float distance, const float effectLength);
+
+	float getPoly6KernelLaplacian(const float distance, const float effectLength);
+
+	Math::Vector3df getSpikyKernelGradient(const Math::Vector3df& distanceVector);
+
+	float getSpikyKernelGradientWeight(const float distance, const float effectLength);
+
+	Math::Vector3df getSpikyKernelGradient(const Math::Vector3df& distanceVector, const float effectLength);
+
+	float getViscosityKernelLaplacian(const float distance, const float effectLength);
+
+	float getCubicSpline(const float q);
+
+	float getCubicSpline(const float distance, const float effectRadius);
+
+	Math::Vector3df getCubicSplineGradient(const Math::Vector3df& distanceVector, const float effectRadius);
+
+	float getCubicSpline(const Math::Vector3df& v, const float effectLength);
+
+	float getEffectLength() const { return effectLength; }
+
+private:
+	float effectLength;
+	float poly6KernelConstant;
+	float spikyKernelGradConstant;
+	float effectLengthSquared;
+};
+
+	}
+}
