09ab8c235add705d0e038cc5790db585d6e74a4e
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2020-06-18 14:28:23 +0900

    implement search.

--- CrystalAlgo/CrystalAlgo/ZIndexedSearchAlgo.cpp
+++ CrystalAlgo/CrystalAlgo/ZIndexedSearchAlgo.cpp
@@ -1,17 +1,62 @@
 #include "ZIndexedSearchAlgo.h"
 
+#include "../../Crystal/Shape/IPoint.h"
+
+using namespace Crystal::Shape;
 using namespace Crystal::Search;
 
 namespace {
-	int ToMortonIndex(int n) {
-		n = (n | (n << 8)) & 0x00ff00ff;
-		n = (n | (n << 4)) & 0x0f0f0f0f;
-		n = (n | (n << 2)) & 0x33333333;
-		return (n | (n << 1)) & 0x55555555;
+	/*
+	int toMortonIndex3d(int x) {
+		x = (x | (x << 16)) & 0x030000FF;
+		x = (x | (x << 8))  & 0x0300F00F;
+		x = (x | (x << 4))  & 0x030C30C3;
+		x = (x | (x << 2))  & 0x09249249;
+		return x;
 	}
+	*/
+
+	int toMortonIndex2d(int n) {
+		n = (n | (n << 16)) & 0xffff0000ffff;
+		n = (n | (n << 8))  & 0x000000ff00ff;
+		n = (n | (n << 4))  & 0x00000f0f0f0f;
+		n = (n | (n << 2))  & 0x000033333333;
+		n = (n | (n << 1))  & 0x000055555555;
+		return n;
+	}
+}
+
+ZIndexedSearchAlgo::ZIndexedSearchAlgo(const double searchRadius) :
+	searchRadius(searchRadius)
+{}
+
+void ZIndexedSearchAlgo::add(IPoint* point)
+{
+	const auto position = point->getPosition();
+	const auto ix = static_cast<int>( position.x / searchRadius );
+	const auto iy = static_cast<int>( position.y / searchRadius );
+	const auto iz = static_cast<int>( position.z / searchRadius );
+	const auto index = toIndex1d(ix, iy, iz);
+	ZIndexedParticle zip(index, point);
+	points.push_back(zip);
+}
+
+void ZIndexedSearchAlgo::sort()
+{
+	std::sort(points.begin(), points.end());
 }
 
 int ZIndexedSearchAlgo::toIndex1d(unsigned int x, unsigned int y, unsigned int z)
 {
-	return (ToMortonIndex(x) | (ToMortonIndex(y) << 1));
+	const auto xi = toMortonIndex2d(x);
+	const auto yi = toMortonIndex2d(y);
+	const auto zi = toMortonIndex2d(z);
+	return (xi | (yi << 1) | (zi << 2));
 }
+
+/*
+std::list<IPoint*> ZIndexedSearchAlgo::findNeighbors(IPoint* searchPoint)
+{
+
+}
+*/
--- CrystalAlgo/CrystalAlgo/ZIndexedSearchAlgo.h
+++ CrystalAlgo/CrystalAlgo/ZIndexedSearchAlgo.h
@@ -1,15 +1,47 @@
 #pragma once
 
+#include <vector>
+#include <list>
+
 namespace Crystal {
+	namespace Shape {
+		class IPoint;
+	}
 	namespace Search {
+
+class ZIndexedParticle
+{
+public:
+	ZIndexedParticle(const int zIndex, Shape::IPoint* point) :
+		zIndex(zIndex),
+		point(point)
+	{}
+
+	bool operator<(const ZIndexedParticle& rhs) {
+		return this->zIndex < rhs.zIndex;
+	}
+
+private:
+	int zIndex;
+	Shape::IPoint* point;
+};
 
 class ZIndexedSearchAlgo
 {
 public:
+	explicit ZIndexedSearchAlgo(const double searchRadius);
+
+	void add(Shape::IPoint* point);
+
+	void sort();
+
 	int toIndex1d(unsigned int x, unsigned int y, unsigned int z);
 
-private:
+	//std::list<Shape::IPoint*> findNeighbors(Shape::IPoint* searchPoint);
 
+private:
+	double searchRadius;
+	std::vector<ZIndexedParticle> points;
 };
 	}
 }
