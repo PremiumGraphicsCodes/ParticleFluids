b0c4b95fd3cf0e50a728f9e090f6857f520975fe
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2020-06-18 13:02:38 +0900

    implement morton curve.

--- CrystalAlgo/CrystalAlgo/ZIndexedSearchAlgo.cpp
+++ CrystalAlgo/CrystalAlgo/ZIndexedSearchAlgo.cpp
@@ -2,9 +2,16 @@
 
 using namespace Crystal::Search;
 
+namespace {
+	int ToMortonIndex(int n) {
+		n = (n | (n << 8)) & 0x00ff00ff;
+		n = (n | (n << 4)) & 0x0f0f0f0f;
+		n = (n | (n << 2)) & 0x33333333;
+		return (n | (n << 1)) & 0x55555555;
+	}
+}
+
 int ZIndexedSearchAlgo::toIndex1d(unsigned int x, unsigned int y, unsigned int z)
 {
-	auto yy = y << 1;
-	auto xx = x + yy;
-	return x + yy;
+	return (ToMortonIndex(x) | (ToMortonIndex(y) << 1));
 }
--- CrystalAlgo/CrystalAlgoTest/ZIndexedSearchAlgoTest.cpp
+++ CrystalAlgo/CrystalAlgoTest/ZIndexedSearchAlgoTest.cpp
@@ -11,4 +11,7 @@ TEST(ZIndexedSearchAlgoTest, TestToIndex1d)
 	EXPECT_EQ(1, algo.toIndex1d(1, 0, 0));
 	EXPECT_EQ(2, algo.toIndex1d(0, 1, 0));
 	EXPECT_EQ(3, algo.toIndex1d(1, 1, 0));
+
+	EXPECT_EQ(4, algo.toIndex1d(2, 0, 0));
+	EXPECT_EQ(5, algo.toIndex1d(3, 0, 0));
 }
