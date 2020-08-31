01dd8e74087fd976794d49b4ea4d847e17449f75
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2020-08-31 11:51:16 +0900

    add renderer.

---
+++ Crystal/Shader/TriangleRenderer.cpp
@@ -0,0 +1,95 @@
+#include "TriangleRenderer.h"
+
+#include <sstream>
+
+using namespace Crystal::Graphics;
+using namespace Crystal::Shader;
+
+namespace {
+	constexpr char* projectionMatrixLabel = "projectionMatrix";
+	constexpr char* modelViewMatrixLabel = "modelviewMatrix";
+}
+
+TriangleRenderer::TriangleRenderer()
+{
+}
+
+bool TriangleRenderer::build(GLObjectFactory& factory)
+{
+	const auto& vsSource = getBuildInVertexShaderSource();
+	const auto& fsSource = getBuiltInFragmentShaderSource();
+
+	const auto isOk = shader.build(vsSource, fsSource);
+
+	shader.findUniformLocation(::projectionMatrixLabel);
+	shader.findUniformLocation(::modelViewMatrixLabel);
+
+	shader.findAttribLocation("position");
+	shader.findAttribLocation("color");
+
+	return isOk;
+}
+
+void TriangleRenderer::release(GLObjectFactory& glFactory)
+{
+
+}
+
+
+void TriangleRenderer::render(const Buffer& buffer)
+{
+	shader.bind();
+	shader.bindOutput("fragColor");
+
+	shader.enableDepthTest();
+
+	shader.sendUniform(::projectionMatrixLabel, buffer.projectionMatrix);
+	shader.sendUniform(::modelViewMatrixLabel, buffer.modelViewMatrix);
+
+	shader.sendVertexAttribute3df("position", buffer.position);
+	shader.sendVertexAttribute4df("color", buffer.color);
+
+	shader.enableVertexAttribute("position");
+	shader.enableVertexAttribute("color");
+
+	shader.drawTriangles(buffer.indices);
+
+	shader.disableVertexAttribute("color");
+	shader.disableVertexAttribute("position");
+
+	shader.disableDepthTest();
+
+	shader.unbind();
+}
+
+std::string TriangleRenderer::getBuildInVertexShaderSource() const
+{
+	std::ostringstream stream;
+	stream
+		<< "#version 150" << std::endl
+		<< "in vec3 position;" << std::endl
+		<< "in vec4 color;" << std::endl
+		<< "out vec4 vColor;" << std::endl
+		<< "uniform mat4 projectionMatrix;" << std::endl
+		<< "uniform mat4 modelviewMatrix;" << std::endl
+		<< "void main(void)" << std::endl
+		<< "{" << std::endl
+		<< "	gl_Position = projectionMatrix * modelviewMatrix * vec4(position, 1.0);" << std::endl
+		<< "	vColor = color;" << std::endl
+		<< "}";
+	return stream.str();
+
+}
+
+std::string TriangleRenderer::getBuiltInFragmentShaderSource() const
+{
+	std::ostringstream stream;
+	stream
+		<< "#version 150" << std::endl
+		<< "in vec4 vColor;" << std::endl
+		<< "out vec4 fragColor;" << std::endl
+		<< "void main(void) {" << std::endl
+		<< "	fragColor = vColor;" << std::endl
+		<< "}" << std::endl;
+	return stream.str();
+}
---
+++ Crystal/Shader/TriangleRenderer.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "ShaderObject.h"
+#include "GLObjectFactory.h"
+#include "VertexBufferObject.h"
+
+namespace Crystal {
+	namespace Shader {
+
+class TriangleRenderer
+{
+public:
+	struct Buffer
+	{
+		Shader::VertexBufferObject position;
+		Shader::VertexBufferObject color;
+		Math::Matrix4dd projectionMatrix;
+		Math::Matrix4dd modelViewMatrix;
+		std::vector<unsigned int> indices;
+	};
+
+	TriangleRenderer();
+
+	bool build(Shader::GLObjectFactory& glFactory);
+
+	void release(Shader::GLObjectFactory& glFactory);
+
+	//void setScene(TriangleShaderScene* scene) { this->scene = scene; }
+
+	void render(const Buffer& buffer);
+
+private:
+	std::string getBuildInVertexShaderSource() const;
+
+	std::string getBuiltInFragmentShaderSource() const;
+
+	Shader::ShaderObject shader;
+};
+
+	}
+}
