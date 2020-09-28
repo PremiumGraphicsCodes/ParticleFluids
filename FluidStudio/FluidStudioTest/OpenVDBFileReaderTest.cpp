dee6e0a6be0b6578d4f206f7d02592b4871b8f1a
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2020-09-28 11:12:35 +0900

    add reader/writer.

---
+++ FluidStudio/FluidStudioTest/OpenVDBFileReaderTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+
+#include "../FluidStudio/OpenVBDFileReader.h"
+
+using namespace Crystal::OpenVDB;
+
+TEST(OpenVDBFileReaderTest, TestRead)
+{
+	/*
+	openvdb::initialize();
+
+	OpenVDBFileReader reader;
+	EXPECT_FALSE( reader.read("testWrite.vdb") );
+	//reader.getPositions();
+	*/
+}
