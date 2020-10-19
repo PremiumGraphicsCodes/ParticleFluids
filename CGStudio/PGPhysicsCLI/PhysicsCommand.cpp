c01240b6421ef0e5b9959969fe993ad262c38303
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2020-10-19 17:43:02 +0900

    fix C++/CLI.

--- CGStudio/PGPhysicsCLI/PhysicsCommand.cpp
+++ CGStudio/PGPhysicsCLI/PhysicsCommand.cpp
@@ -10,56 +10,48 @@
 
 using namespace PG::CLI;
 
-/*
 namespace {
 	Crystal::Command::Command instance;
 }
-*/
 
 PhysicsCommand::PhysicsCommand()
 {}
 
 PhysicsCommand::PhysicsCommand(System::String^ name)
 {
-	command->Create(name);
-	//const auto& str = Converter::toCpp(name);
-	//::instance.create(str);
+	const auto& str = Converter::toCpp(name);
+	::instance.create(str);
 }
 
 void PhysicsCommand::Create(System::String^ name)
 {
-	command->Create(name);
-	//const auto& str = Converter::toCpp(name);
-	//::instance.create(str);
+	const auto& str = Converter::toCpp(name);
+	::instance.create(str);
 }
 
 generic <class T>
 void PhysicsCommand::SetArg(System::String^ name, T value)
 {
-	command->SetArg(name, value);
-	//const auto& str = PG::CLI::Converter::toCpp(name);
-	//const auto v = AnyConverter::toCpp(value, T::typeid);
-	//::instance.setArg(str, v);
+	const auto& str = PG::CLI::Converter::toCpp(name);
+	const auto v = AnyConverter::toCpp(value, T::typeid);
+	::instance.setArg(str, v);
 }
 
 bool PhysicsCommand::Execute(WorldAdapter^ objects)
 {
 	auto world = static_cast<Crystal::Scene::World*>( objects->getPtr().ToPointer() );
-	return command->Execute(objects);
-	//return ::instance.execute(objects->instance);
+	return ::instance.execute(world);
 }
 
 generic <class T>
 T PhysicsCommand::GetResult(System::String^ name)
 {
-	return command->GetResult<T>(name);
-	//const auto& str = msclr::interop::marshal_as<std::string>(name);
-	//auto result = ::instance.getResult(str);
-	//return (T)(AnyConverter::fromCpp(result));
+	const auto& str = msclr::interop::marshal_as<std::string>(name);
+	auto result = ::instance.getResult(str);
+	return (T)(AnyConverter::fromCpp(result));
 }
 
 void PhysicsCommand::Clear()
 {
-	command->Clear();
-	//::instance.clear();
+	::instance.clear();
 }
--- CGStudio/PGPhysicsCLI/PhysicsCommand.h
+++ CGStudio/PGPhysicsCLI/PhysicsCommand.h
@@ -24,7 +24,6 @@ public:
 	void Clear();
 
 private:
-	Command^ command;
 };
 
 
