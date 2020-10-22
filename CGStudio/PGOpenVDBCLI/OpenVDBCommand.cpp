5e1cc75f5a26368344ca47118e146994329452dc
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2020-10-22 14:55:29 +0900

    update command.

--- CGStudio/PGOpenVDBCLI/OpenVDBCommand.cpp
+++ CGStudio/PGOpenVDBCLI/OpenVDBCommand.cpp
@@ -3,6 +3,8 @@
 
 #include "../../CrystalViewer/Command/Command.h"
 
+#include "../../FluidStudio/OpenVDBCommand/OpenVDBCommandFactory.h"
+
 //#include "../../Crystal/Scene/World.h"
 #include "../PGCLI/Converter.h"
 #include "../PGCLI/AnyConverter.h"
@@ -11,7 +13,8 @@
 using namespace PG::CLI;
 
 namespace {
-	Crystal::Command::Command instance;
+	std::unique_ptr<Crystal::Command::ICommand> instance;
+	Crystal::OpenVDB::OpenVDBCommandFactory factory;
 }
 
 OpenVDBCommand::OpenVDBCommand()
@@ -19,44 +22,39 @@ OpenVDBCommand::OpenVDBCommand()
 
 OpenVDBCommand::OpenVDBCommand(System::String^ name)
 {
-	command->Create(name);
-	//const auto& str = Converter::toCpp(name);
-	//::instance.create(str);
+	const auto& str = Converter::toCpp(name);
+	::instance = factory.createCommand(str);
 }
 
 void OpenVDBCommand::Create(System::String^ name)
 {
-	command->Create(name);
-	//const auto& str = Converter::toCpp(name);
-	//::instance.create(str);
+	const auto& str = Converter::toCpp(name);
+	::instance = factory.createCommand(str);
 }
 
 generic <class T>
 void OpenVDBCommand::SetArg(System::String^ name, T value)
 {
-	command->SetArg(name, value);
-	//const auto& str = PG::CLI::Converter::toCpp(name);
-	//const auto v = AnyConverter::toCpp(value, T::typeid);
-	//::instance.setArg(str, v);
+	const auto& str = PG::CLI::Converter::toCpp(name);
+	const auto v = AnyConverter::toCpp(value, T::typeid);
+	::instance->setArg(str, v);
 }
 
 bool OpenVDBCommand::Execute(WorldAdapter^ objects)
 {
-	return command->Execute(objects);
-	//return ::instance.execute(objects->instance);
+	auto world = static_cast<Crystal::Scene::World*>(objects->getPtr().ToPointer());
+	return ::instance->execute(world);
 }
 
 generic <class T>
 T OpenVDBCommand::GetResult(System::String^ name)
 {
-	return command->GetResult<T>(name);
-	//const auto& str = msclr::interop::marshal_as<std::string>(name);
-	//auto result = ::instance.getResult(str);
-	//return (T)(AnyConverter::fromCpp(result));
+	const auto& str = msclr::interop::marshal_as<std::string>(name);
+	auto result = ::instance->getResult(str);
+	return (T)(AnyConverter::fromCpp(result));
 }
 
 void OpenVDBCommand::Clear()
 {
-	command->Clear();
-	//::instance.clear();
+	::instance.reset();
 }
