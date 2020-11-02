dd4829fe8468117e7e50533465c76af348c1fc91
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2020-11-02 09:41:02 +0900

    add label.

---
+++ CrystalAlgo/CrystalPhysicsCommand/PublicLabels/FluidSimulationLabels.h
@@ -0,0 +1,10 @@
+#include "../../CrystalViewer/Command/Public/PublicLabel.h"
+
+namespace {
+	PublicLabel CommandNameLabel = "FluidSceneCreateCommand";
+	PublicLabel ParticleSystemIdLabel = "ParticleSystemId";
+	PublicLabel StiffnessLabel = "Stiffness";
+	PublicLabel ViscosityLabel = "Viscosity";
+	PublicLabel NameLabel = "Name";
+	PublicLabel NewIdLabel = "NewId";
+}
