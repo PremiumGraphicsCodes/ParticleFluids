db1c855fc3851fef72e9c5fd83345dee29870133
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2020-11-09 15:25:07 +0900

    add view model.

---
+++ CGStudio/FluidStudio/Physics/Solver/SolverSceneViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Regions;
+using System;
+
+namespace FluidStudio.Physics.Solver
+{
+    public class SolverSceneViewModel : INavigationAware
+    {
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            var item = navigationContext.Parameters["Scene"] as SolverScene;
+            if (item == null)
+            {
+                return;
+            }
+            //Stiffness.Value = item.Stiffness;
+            //Viscosity.Value = item.Viscosity;
+
+        }
+    }
+}
