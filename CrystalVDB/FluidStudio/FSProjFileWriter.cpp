53d5b772a2e08aeb617ebca2e2c61b228f277317
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2020-11-16 12:41:39 +0900

    remove unused.

--- CrystalVDB/FluidStudio/FSProjFileWriter.cpp
+++
@@ -1,8 +0,0 @@
-#include "FSProjFileWriter.h"
-
-using namespace PG::FS;
-
-//nlohmann::json FSProjFileWriter::write(const FSScene& scene)
-//{
-//	return { {"MaxTimeStep", scene.args.maxTimeStep } };
-//}
