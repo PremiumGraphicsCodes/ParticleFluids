fce131e511f769ea168775d7d5b9682bfe9d1475
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2020-12-29 09:25:46 +0900

    remove unused project.

--- CrystalVDB/VDBConverterTest/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "gtest/gtest.h"
-#include "../VDBConverter/VDBAdapter.h"
-
-int main(int argc, char** argv) {
-//	std::cout << "hello" << std::endl;
-	::testing::InitGoogleTest(&argc, argv);
-	Crystal::VDB::VDBAdapter::init();
-	return RUN_ALL_TESTS();
-}
