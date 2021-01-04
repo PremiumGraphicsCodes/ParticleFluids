2044bc46b84d15375a4fedd2e7a4aaeeb08ad434
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2021-01-04 15:13:17 +0900

    add class.

--- Crystal/IO/OBJFileReader.cpp
+++ Crystal/IO/OBJFileReader.cpp
@@ -19,6 +19,52 @@ using namespace Crystal::Graphics;
 using namespace Crystal::Shape;
 using namespace Crystal::IO;
 
+class OBJSyntaxParser
+{
+public:
+
+	static OBJFace parseFace(std::vector< std::string >& strs)
+	{
+		OBJFace face;
+		for (int i = 0; i < strs[i].size(); ++i) {
+			if (strs[i].empty()) {
+				continue;
+			}
+			std::string::size_type pos(strs[i].find("//"));
+			if (pos != std::string::npos) {
+				strs[i].replace(pos, 2, "/ /");
+			}
+
+			std::vector<std::string>& splitted = Helper::split(strs[i], '/');
+			const int positionIndex = std::stoi(splitted[0]);
+			face.positionIndices.push_back(positionIndex);
+
+			if (splitted.size() >= 2 && splitted[1] != " ") {
+				const int texIndex = std::stoi(splitted[1]);
+				face.texCoordIndices.push_back(texIndex);
+			}
+			else {
+				face.texCoordIndices.push_back(-1);
+			}
+
+			if (splitted.size() >= 3) {
+				const int normalIndex = std::stoi(splitted[2]);
+				face.normalIndices.push_back(normalIndex);
+			}
+			else {
+				face.normalIndices.push_back(-1);
+			}
+		}
+		return face;
+	}
+
+	static OBJFace parseFaceLine(const std::string& line)
+	{
+		std::vector< std::string >& strs = Helper::split(line, ' ');
+		return parseFace(strs);
+	}
+};
+
 bool OBJFileReader::read(const std::filesystem::path& filePath)
 {
 	std::ifstream stream;
@@ -68,40 +114,9 @@ bool OBJFileReader::read(std::istream& stream)
 		else if (header == "f") {
 			std::getline(stream, str);
 
-			std::vector< std::string >& strs = Helper::split(str, ' ');
+			//std::vector< std::string >& strs = Helper::split(str, ' ');
 
-			OBJFace face;
-
-			//assert(strs.front() == "f");
-			for (unsigned int i = 0; i < strs.size(); ++i) {
-				if (strs[i].empty()) {
-					continue;
-				}
-				std::string::size_type pos(strs[i].find("//"));
-				if (pos != std::string::npos) {
-					strs[i].replace(pos, 2, "/ /");
-				}
-
-				std::vector<std::string>& splitted = Helper::split(strs[i], '/');
-				const int positionIndex = std::stoi(splitted[0]);
-				face.positionIndices.push_back(positionIndex);
-
-				if (splitted.size() >= 2 && splitted[1] != " ") {
-					const int texIndex = std::stoi(splitted[1]);
-					face.texCoordIndices.push_back(texIndex);
-				}
-				else {
-					face.texCoordIndices.push_back(-1);
-				}
-
-				if (splitted.size() >= 3) {
-					const int normalIndex = std::stoi(splitted[2]);
-					face.normalIndices.push_back(normalIndex);
-				}
-				else {
-					face.normalIndices.push_back(-1);
-				}
-			}
+			OBJFace face = OBJSyntaxParser::parseFaceLine(str);
 
 			currentGroup.faces.push_back(face);
 		}
