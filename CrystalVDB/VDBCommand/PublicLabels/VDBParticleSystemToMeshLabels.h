cc8d14ee5e8188adf6c975c1eb3e036f81aefaa4
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2021-01-14 14:05:50 +0900

    update cs.

--- CrystalVDB/VDBCommand/PublicLabels/VDBParticleSystemToMeshLabels.h
+++
@@ -1,9 +0,0 @@
-#include "../../../CrystalViewer/Command/Public/PublicLabel.h"
-
-namespace
-{
-	PublicLabel CommandNameLabel = "VDBParticleSystemToMesh";
-	PublicLabel ParticleSystemIdLabel = "ParticleSystemId";
-	PublicLabel VDBMeshIdLabel = "VDBMeshId";
-	PublicLabel RadiusLabel = "Radius";
-}
