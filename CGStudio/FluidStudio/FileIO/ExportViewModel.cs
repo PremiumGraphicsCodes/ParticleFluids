a6f2af41658a4ae2e1a8e839832149a0a68b04a1
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2021-01-18 12:33:24 +0900

    add export view model.

---
+++ CGStudio/FluidStudio/FileIO/ExportViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using PG.Control.OpenGL;
+using PG.Control.UI;
+using PG.Scene;
+using Reactive.Bindings;
+
+namespace FluidStudio.FileIO
+{
+    public class ExportViewModel
+    {
+        public SceneSelectViewModel SceneSelectViewModel { get; }
+
+        public ReactiveProperty<string> FilePath { get; }
+            = new ReactiveProperty<string>("");
+
+        public ReactiveCommand SelectCommand { get; }
+            = new ReactiveCommand();
+
+        public ExportViewModel(SceneList scenes, Canvas3d canvas)
+        {
+            SceneSelectViewModel = new SceneSelectViewModel(scenes, canvas);
+            SelectCommand.Subscribe(OnSelect);
+        }
+
+        private void OnSelect()
+        {
+            var dialog = new SaveFileDialog
+            {
+                Title = "Export",
+                Filter = "OpenVDBFile(*.vdb)|*.vdb|AllFiles(*.*)|*.*",
+            };
+            var isOk = dialog.ShowDialog();
+            if(isOk == true)
+            {
+                this.FilePath.Value = dialog.FileName;
+            }
+        }
+    }
+}
