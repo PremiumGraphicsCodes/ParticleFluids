cd83c9440c6671f7144f303c6e53de147032d824
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2021-02-04 13:52:11 +0900

    add PSToVolume.

---
+++ FluidStudio/FluidStudio/Tool/Modeling/PSToVolumeViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Reactive.Bindings;
+
+namespace FluidStudio.Tool.Modeling
+{
+    public class PSToVolumeViewModel
+    {
+        public ReactiveCommand VDBInputDirectorySelectCommand { get; }
+            = new ReactiveCommand();
+
+        public ReactiveCommand VDBOutputDirectorySelectCommand { get; }
+            = new ReactiveCommand();
+
+        public ReactiveProperty<string> VDBInputDirectoryPath { get; }
+            = new ReactiveProperty<string>("");
+
+        public ReactiveProperty<string> VDBOutputDirectoryPath { get; }
+            = new ReactiveProperty<string>("");
+
+        public ReactiveCommand StartCommand { get; }
+            = new ReactiveCommand();
+
+        public PSToVolumeViewModel()
+        {
+            this.VDBInputDirectorySelectCommand.Subscribe(OnSelectVDBImportDirectory);
+            this.VDBOutputDirectorySelectCommand.Subscribe(OnSelectVDBExportDirectory);
+            this.StartCommand.Subscribe(OnStart);
+        }
+
+        private void OnSelectVDBImportDirectory()
+        {
+            var dialog = new Microsoft.WindowsAPICodePack.Dialogs.CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            var result = dialog.ShowDialog();
+            if (result == Microsoft.WindowsAPICodePack.Dialogs.CommonFileDialogResult.Ok)
+            {
+                this.VDBInputDirectoryPath.Value = dialog.FileName;
+            }
+        }
+
+        private void OnSelectVDBExportDirectory()
+        {
+            var dialog = new Microsoft.WindowsAPICodePack.Dialogs.CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            var result = dialog.ShowDialog();
+            if (result == Microsoft.WindowsAPICodePack.Dialogs.CommonFileDialogResult.Ok)
+            {
+                this.VDBOutputDirectoryPath.Value = dialog.FileName;
+            }
+        }
+
+        private void OnStart()
+        {
+            // vdbファイルを列挙する．
+            // 一つ一つコンバートしてexportDirectoryに保存する．
+        }
+    }
+}
