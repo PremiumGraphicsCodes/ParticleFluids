c47f28f076803ae0cf47f27bd4abe56c541acf8f
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2021-03-11 10:04:54 +0900

    update batch.

--- FluidStudio/Release.bat
+++ FluidStudio/Release.bat
@@ -6,6 +6,7 @@ set mm=%HIZUKE:~5,2%
 set dd=%HIZUKE:~8,2%
 set name=FS%yy%%mm%%dd%
 mkdir %name%
+cp readme.txt %name%
 cp -r Licenses %name%
 cp x64/Release/*.dll %name%
 cp x64/Release/*.exe %name%
