dac188c404f658733cb3fa1f49defdc136a6d08e
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2021-03-11 11:14:40 +0900

    update release script.

--- FluidStudio/Release.bat
+++ FluidStudio/Release.bat
@@ -6,8 +6,11 @@ set mm=%HIZUKE:~5,2%
 set dd=%HIZUKE:~8,2%
 set name=FS%yy%%mm%%dd%
 mkdir %name%
+cd %name%
+mkdir bin
+cd ../
 cp readme.txt %name%
 cp -r Licenses %name%
-cp x64/Release/*.dll %name%
-cp x64/Release/*.exe %name%
+cp x64/Release/*.dll %name%/bin
+cp x64/Release/*.exe %name%/bin
 powershell compress-archive %name% %name%.zip
