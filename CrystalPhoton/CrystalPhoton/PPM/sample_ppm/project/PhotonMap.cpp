16dce2a47aec3d78379a1394d933d0c394f3aec1
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2021-03-18 10:58:12 +0900

    refactor.

--- CrystalPhoton/CrystalPhoton/PPM/sample_ppm/project/PhotonMap.cpp
+++ CrystalPhoton/CrystalPhoton/PPM/sample_ppm/project/PhotonMap.cpp
@@ -1,78 +1,12 @@
 #include "PhotonMap.h"
 
+#include "SpaceHash.h"
+
 #include <chrono>
 
 using namespace Crystal::Photon;
 
 namespace {
-    constexpr double ALPHA = 0.7; // the alpha parameter of PPM
-
-    inline unsigned int hash
-    (
-        const int ix,
-        const int iy,
-        const int iz,
-        const Scene& scene
-    )
-    {
-        return (unsigned int)(
-            (ix * 73856093) ^
-            (iy * 19349663) ^
-            (iz * 83492791)) % scene.hash_grid.size();
-    }
-    void build_hash_grid
-    (
-        const int w,
-        const int h,
-        Scene& scene
-    )
-    {
-        // heuristic for initial radius
-        auto size = scene.hpbbox.maxi - scene.hpbbox.mini;
-        auto irad = ((size.x + size.y + size.z) / 3.0) / ((w + h) / 2.0) * 2.0;
-
-        // determine hash table size
-        // we now find the bounding box of all the measurement points inflated by the initial radius
-        scene.hpbbox.reset();
-        auto photon_count = 0;
-        for (auto itr = scene.hitpoints.begin(); itr != scene.hitpoints.end(); ++itr)
-        {
-            auto hp = (*itr);
-            hp->r2 = irad * irad;
-            hp->n = 0;
-            hp->flux = Vector3();
-
-            photon_count++;
-            scene.hpbbox.merge(hp->pos - irad);
-            scene.hpbbox.merge(hp->pos + irad);
-        }
-
-        // make each grid cell two times larger than the initial radius
-        scene.hash_s = 1.0 / (irad * 2.0);
-
-        // build the hash table
-        scene.hash_grid.resize(photon_count);
-        scene.hash_grid.shrink_to_fit();
-        for (auto itr = scene.hitpoints.begin(); itr != scene.hitpoints.end(); ++itr)
-        {
-            auto hp = (*itr);
-            auto min = ((hp->pos - irad) - scene.hpbbox.mini) * scene.hash_s;
-            auto max = ((hp->pos + irad) - scene.hpbbox.mini) * scene.hash_s;
-
-            for (int iz = abs(int(min.z)); iz <= abs(int(max.z)); iz++)
-            {
-                for (int iy = abs(int(min.y)); iy <= abs(int(max.y)); iy++)
-                {
-                    for (int ix = abs(int(min.x)); ix <= abs(int(max.x)); ix++)
-                    {
-                        int hv = hash(ix, iy, iz, scene);
-                        scene.hash_grid[hv].push_back(hp);
-                    }
-                }
-            }
-        }
-    }
-
 
     inline bool intersect(const Ray& r, double& t, int& id, Scene& scene)
     {
@@ -175,7 +109,7 @@ void PhotonMap::trace_photon_ray(const Ray& r, int dpt, const Vector3& fl, const
             // rarely accumulated to the same measurement points at the same time (especially with QMC).
             // it is also significantly faster.
             {
-                auto list = scene.hash_grid[hash(ix, iy, iz, scene)];
+                auto list = scene.hash_grid[SpaceHash::hash(ix, iy, iz, scene)];
                 for (auto itr = list.begin(); itr != list.end(); itr++)
                 {
                     auto hp = (*itr);
--- CrystalPhoton/CrystalPhoton/PPM/sample_ppm/project/main.cpp
+++ CrystalPhoton/CrystalPhoton/PPM/sample_ppm/project/main.cpp
@@ -20,86 +20,11 @@
 #include "hitrecord.h"
 
 #include "Scene.h"
+#include "SpaceHash.h"
 
 #include <array>
 
 using namespace Crystal::Photon;
-
-constexpr double ALPHA = 0.7; // the alpha parameter of PPM
-
-
-//-------------------------------------------------------------------------------------------
-//      ハッシュキーを取得します.
-//-------------------------------------------------------------------------------------------
-inline unsigned int hash
-(
-    const int ix,
-    const int iy,
-    const int iz,
-    const Scene& scene
-) 
-{
-    return (unsigned int)(
-        (ix * 73856093) ^
-        (iy * 19349663) ^
-        (iz * 83492791)) % scene.hash_grid.size();
-}
-
-//-------------------------------------------------------------------------------------------
-//      ハッシュグリッドを構築します.
-//-------------------------------------------------------------------------------------------
-void build_hash_grid
-(
-    const int w,
-    const int h,
-    Scene& scene
-)
-{
-    // heuristic for initial radius
-    auto size = scene.hpbbox.maxi - scene.hpbbox.mini;
-    auto irad = ((size.x + size.y + size.z) / 3.0) / ((w + h) / 2.0) * 2.0;
-
-    // determine hash table size
-    // we now find the bounding box of all the measurement points inflated by the initial radius
-    scene.hpbbox.reset();
-    auto photon_count = 0;
-    for( auto itr = scene.hitpoints.begin(); itr != scene.hitpoints.end(); ++itr )
-    {
-        auto hp  = (*itr);
-        hp->r2   = irad *irad;
-        hp->n    = 0;
-        hp->flux = Vector3();
-
-        photon_count++;
-        scene.hpbbox.merge( hp->pos - irad );
-        scene.hpbbox.merge( hp->pos + irad );
-    }
-
-    // make each grid cell two times larger than the initial radius
-    scene.hash_s = 1.0 / (irad * 2.0);
-
-    // build the hash table
-    scene.hash_grid.resize( photon_count );
-    scene.hash_grid.shrink_to_fit();
-    for( auto itr = scene.hitpoints.begin(); itr != scene.hitpoints.end(); ++itr )
-    {
-        auto hp = (*itr);
-        auto min = ((hp->pos - irad) - scene.hpbbox.mini) * scene.hash_s;
-        auto max = ((hp->pos + irad) - scene.hpbbox.mini) * scene.hash_s;
-
-        for (int iz = abs(int(min.z)); iz <= abs(int(max.z)); iz++)
-        {
-            for (int iy = abs(int(min.y)); iy <= abs(int(max.y)); iy++)
-            {
-                for (int ix = abs(int(min.x)); ix <= abs(int(max.x)); ix++)
-                {
-                    int hv = hash( ix, iy, iz, scene );
-                    scene.hash_grid[ hv ].push_back( hp );
-                }
-            }
-        }
-    }
-}
 
 //-------------------------------------------------------------------------------------------
 //      交差判定を行います.
@@ -155,7 +80,7 @@ public:
         start = std::chrono::system_clock::now();
 
         // build the hash table over the measurement points
-        build_hash_grid(w, h, scene);
+        SpaceHash::build_hash_grid(w, h, scene);
 
         end = std::chrono::system_clock::now();
         dif = end - start;
