fa48f2c00931a5ef4eb7cd184bbfe1267725fe38
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2021-04-05 10:52:27 +0900

    add label.

---
+++ FluidStudio/FluidStudio/SpaceLabels.cs
@@ -0,0 +1,12 @@
+namespace PG {
+	public class SparseVolumeSceneCreateLabels
+	{
+		public const string CommandNameLabel = "SparseVolumeSceneCreate";
+		public const string ResolutionLabel = "Resolution";
+		public const string BoundingBoxLabel = "BoundingBox";
+		public const string NameLabel = "Name";
+		public const string LayerLabel = "Layer";
+		public const string NewIdLabel = "NewId";
+	}
+
+}
