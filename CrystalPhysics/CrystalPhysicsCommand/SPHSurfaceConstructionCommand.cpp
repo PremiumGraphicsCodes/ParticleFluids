4328075147c5c006a16f75855ad84eff10f737d7
SatoshiMabuchiLW <s.mabuchi@linkwiz.co.jp>
2021-04-05 09:50:07 +0900

    implement surface command.

---
+++ CrystalPhysics/CrystalPhysicsCommand/SPHSurfaceConstructionCommand.cpp
@@ -0,0 +1,58 @@
+#include "SPHSurfaceConstructionCommand.h"
+
+#include "PublicLabels/SPHSurfaceConstructionLabels.h"
+
+//#include "../CrystalPhysics/KFFluidScene.h"
+#include "../../Crystal/Scene/ParticleSystemScene.h"
+#include "../../CrystalSpace/CrystalSpace/SparseVolumeScene.h"
+
+#include "../../CrystalPhysics/CrystalPhysics/SPHSurfaceBuilder.h"
+
+using namespace Crystal::Math;
+using namespace Crystal::Space;
+using namespace Crystal::Physics;
+using namespace Crystal::Scene;
+
+std::string SPHSurfaceConstructionCommand::getName()
+{
+	return ::CommandNameLabel;
+}
+
+SPHSurfaceConstructionCommand::Args::Args() :
+	particleSystemId(::ParticleSystemIdLabel, -1),
+	sparseVolumeId(::SparseVolumeIdLabel, -1),
+	effectLength(::EffectLengthLabel, 1.0),
+	cellLength(::CellLengthLabel, 0.5)
+{
+}
+
+SPHSurfaceConstructionCommand::Results::Results()// :
+//	newId(::NewIdLabel, 0)
+{
+//	add(&newId);
+}
+
+SPHSurfaceConstructionCommand::SPHSurfaceConstructionCommand() :
+	ICommand(&args, &results)
+{}
+
+bool SPHSurfaceConstructionCommand::execute(World* world)
+{
+	auto ps = world->getScenes()->findSceneById<ParticleSystemScene*>(args.particleSystemId.getValue());
+	if (ps == nullptr) {
+		return false;
+	}
+	auto sp = world->getScenes()->findSceneById<SparseVolumeScene*>(args.sparseVolumeId.getValue());
+	if (sp == nullptr) {
+		return false;
+	}
+
+	auto positions = ps->getPositions();
+
+	SPHSurfaceBuilder builder;
+	builder.buildAnisotoropic(positions, args.effectLength.getValue(), args.cellLength.getValue());
+	auto shape = builder.getVolume();
+	//sp->
+	//results.newId.setValue(fluidScene->getId());
+	return true;
+}
---
+++ CrystalPhysics/CrystalPhysicsCommand/SPHSurfaceConstructionCommand.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "../../CrystalViewer/Command/ICommand.h"
+
+namespace Crystal {
+	namespace Physics {
+
+class SPHSurfaceConstructionCommand : public Command::ICommand
+{
+public:
+	struct Args : Command::IArgs
+	{
+		Command::Arg<int> particleSystemId;
+		Command::Arg<int> sparseVolumeId;
+
+		Command::Arg<double> effectLength;
+		Command::Arg<double> cellLength;
+
+		Args();
+	};
+
+	struct Results : Command::IResults
+	{
+//		Command::Result<int> newId;
+
+		Results();
+	};
+
+	static std::string getName();
+
+	std::string getCommandName() const { return getName(); }
+
+	SPHSurfaceConstructionCommand();
+
+	bool execute(Scene::World* world) override;
+
+private:
+	Args args;
+	Results results;
+};
+
+	}
+}
