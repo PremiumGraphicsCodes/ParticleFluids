e1b4cd8d9867daff9db94d0fe3c14a6fa6d3def1
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2021-04-26 13:39:58 +0900

    refactor.

--- CrystalPhysics/CrystalPhysics/KFFluidScene.cpp
+++ CrystalPhysics/CrystalPhysics/KFFluidScene.cpp
@@ -4,50 +4,8 @@ using namespace Crystal::Math;
 using namespace Crystal::Scene;
 using namespace Crystal::Physics;
 
-IKFFluidScene::IKFFluidScene(const int id, const std::string& name) :
-	IParticleSystemScene(id, name),
-	pressureCoe(10000.0),
-	viscosityCoe(50.0)
-{
-	this->controller = std::make_unique<KFFluidScenePresenter>(this);
-}
-
 KFFluidScene::KFFluidScene(const int id, const std::string& name) :
 	IKFFluidScene(id, name),
 	isBoundary_(false)
 {
-}
-
-IKFFluidScene::~IKFFluidScene()
-{
-	clearParticles();
-}
-
-void IKFFluidScene::clearParticles()
-{
-	for (auto p : particles) {
-		delete p;
-	}
-	particles.clear();
-}
-
-Box3d IKFFluidScene::getBoundingBox() const
-{
-	if (particles.empty()) {
-		return Box3d::createDegeneratedBox();
-	}
-	Box3d bb(particles.front()->getPosition());
-	for (auto p : particles) {
-		bb.add(p->getPosition());
-	}
-	return bb;
-}
-
-std::vector<Vector3dd> IKFFluidScene::getPositions() const
-{
-	std::vector<Vector3dd> positions;
-	for (auto p : particles) {
-		positions.push_back(p->getPosition());
-	}
-	return positions;
 }
--- CrystalPhysics/CrystalPhysics/KFFluidScene.h
+++ CrystalPhysics/CrystalPhysics/KFFluidScene.h
@@ -1,58 +1,9 @@
 #pragma once
 
-#include "../../Crystal/Scene/IParticleSystemScene.h"
-
-#include "KFMacroParticle.h"
-#include "KFFluidScenePresenter.h"
+#include "IKFFluidScene.h"
 
 namespace Crystal {
 	namespace Physics {
-		class KFFluidScenePresenter;
-
-class IKFFluidScene : public Scene::IParticleSystemScene
-{
-public:
-	IKFFluidScene(const int id, const std::string& name);
-
-	virtual ~IKFFluidScene();
-
-	void addParticle(KFMacroParticle* mp) {
-		mp->setPressureCoe(this->pressureCoe);
-		mp->setViscosityCoe(this->viscosityCoe);
-		particles.push_back(mp);
-	}
-
-	std::vector<KFMacroParticle*> getParticles() const { return particles; }
-
-	void clearParticles();
-
-	Math::Box3d getBoundingBox() const override;
-
-	void setPressureCoe(const float coe) { this->pressureCoe = coe; }
-
-	float getPressure() const { return this->pressureCoe; }
-
-	void setViscosityCoe(const float coe) { this->viscosityCoe = coe; }
-
-	float getViscosityCoe() const { return this->viscosityCoe; }
-
-
-	void translate(const Math::Vector3dd& v) override { ; }
-
-	void transform(const Math::Matrix3dd& m) override { ; }
-
-	void transform(const Math::Matrix4dd& m) override { ; }
-
-	std::vector<Math::Vector3dd> getPositions() const override;
-
-	Scene::IPresenter* getPresenter() { return controller.get(); }
-
-private:
-	std::unique_ptr<KFFluidScenePresenter> controller;
-	std::vector<KFMacroParticle*> particles;
-	float pressureCoe;
-	float viscosityCoe;
-};
 
 class KFFluidScene : public IKFFluidScene
 {
