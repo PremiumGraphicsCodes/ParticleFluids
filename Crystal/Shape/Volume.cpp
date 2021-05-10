a474c75ce04a8d1e4b635779cd45dfe5a6623d0c
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2021-05-10 09:27:02 +0900

    use template.

--- Crystal/Shape/Volume.cpp
+++ Crystal/Shape/Volume.cpp
@@ -2,3 +2,51 @@
 
 using namespace Crystal::Math;
 using namespace Crystal::Shape;
+
+template<typename T>
+Volume<T>::Volume(const Math::Box3d& box, const std::array<size_t, 3>& resolutions) :
+	box(box),
+	resolutions(resolutions)
+{
+	nodes.resize(resolutions[0]);
+	for (int i = 0; i < resolutions[0]; ++i) {
+		nodes[i].resize(resolutions[1]);
+		for (int j = 0; j < resolutions[1]; ++j) {
+			nodes[i][j].resize(resolutions[2]);
+		}
+	}
+}
+
+template<typename T>
+Vector3dd Volume<T>::getCellPosition(int i, int j, int k) const
+{
+	const auto& length = getCellLength();
+	const auto cx = length.x * i;
+	const auto cy = length.y * j;
+	const auto cz = length.z * k;
+	return box.getMin() + length * 0.5 + Math::Vector3dd(cx, cy, cz);
+}
+
+template<typename T>
+std::array<size_t, 3> Volume<T>::getIndexFromPosition(const Vector3dd& position)
+{
+	const auto p = position - box.getMin();
+	const auto length = getCellLength();
+	const auto ix = static_cast<size_t>(p.x / length.x);
+	const auto iy = static_cast<size_t>(p.y / length.y);
+	const auto iz = static_cast<size_t>(p.z / length.z);
+	return { ix, iy, iz };
+}
+
+template<typename T>
+Vector3dd Volume<T>::getCellLength() const
+{
+	const auto& length = box.getLength();
+	const auto dx = length.x / static_cast<double>(resolutions[0]);
+	const auto dy = length.y / static_cast<double>(resolutions[1]);
+	const auto dz = length.z / static_cast<double>(resolutions[2]);
+	return Math::Vector3dd(dx, dy, dz);
+}
+
+template class Volume<float>;
+template class Volume<double>;
--- Crystal/Shape/Volume.h
+++ Crystal/Shape/Volume.h
@@ -23,25 +23,7 @@ template<typename T>
 class Volume : public IShape
 {
 public:
-	Volume(const Math::Box3d& box, const std::array<size_t, 3>& resolutions) :
-		box(box),
-		resolutions(resolutions)
-	{
-		nodes.resize(resolutions[0]);
-		for (int i = 0; i < resolutions[0]; ++i) {
-			nodes[i].resize(resolutions[1]);
-			for (int j = 0; j < resolutions[1]; ++j) {
-				nodes[i][j].resize(resolutions[2]);
-			}
-		}
-	}
-
-	/*
-	Volume(const int unum, const int vnum, const int wnum)
-	{
-		build(unum, vnum, wnum);
-	}
-	*/
+	Volume(const Math::Box3d& box, const std::array<size_t, 3>& resolutions);
 
 	~Volume() {
 		//clear();
@@ -63,31 +45,11 @@ public:
 
 	std::array<size_t,3> getResolutions() const { return resolutions; }
 
-	Math::Vector3dd getCellPosition(int i, int j, int k) const {
-		const auto& length = getCellLength();
-		const auto cx = length.x * i;
-		const auto cy = length.y * j;
-		const auto cz = length.z * k;
-		return box.getMin() + length * 0.5 + Math::Vector3dd(cx, cy, cz);
-	}
+	Math::Vector3dd getCellPosition(int i, int j, int k) const;
 
-	std::array<size_t, 3> getIndexFromPosition(const Math::Vector3dd& position)
-	{
-		const auto p = position - box.getMin();
-		const auto length = getCellLength();
-		const auto ix = static_cast<size_t>( p.x / length.x );
-		const auto iy = static_cast<size_t>( p.y / length.y );
-		const auto iz = static_cast<size_t>( p.z / length.z );
-		return { ix, iy, iz };
-	}
+	std::array<size_t, 3> getIndexFromPosition(const Math::Vector3dd& position);
 
-	Math::Vector3dd getCellLength() const {
-		const auto& length = box.getLength();
-		const auto dx = length.x / static_cast<double>(resolutions[0]);
-		const auto dy = length.y / static_cast<double>(resolutions[1]);
-		const auto dz = length.z / static_cast<double>(resolutions[2]);
-		return Math::Vector3dd(dx, dy, dz);
-	}
+	Math::Vector3dd getCellLength() const;
 
 	T getValue(const int i, const int j, const int k) const { return nodes[i][j][k]; }
 
