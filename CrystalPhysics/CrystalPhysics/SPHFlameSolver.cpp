14bec5d585658118e75cbd80ef03f8ecfd2b9fe8
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2021-06-03 13:14:59 +0900

    add solver.

---
+++ CrystalPhysics/CrystalPhysics/SPHFlameSolver.cpp
@@ -0,0 +1,85 @@
+#include "SPHFlameSolver.h"
+#include "SPHFlameParticle.h"
+//#include "CSPHBoundarySolver.h"
+#include "SPHFlameScene.h"
+
+#include "../../CrystalSpace/CrystalSpace/IndexedSortSearchAlgo.h"
+
+using namespace Crystal::Math;
+using namespace Crystal::Space;
+using namespace Crystal::Physics;
+
+void SPHFlameSolver::clear()
+{
+	//timeStep = 0.001;
+}
+
+void SPHFlameSolver::step()
+{
+	simulate(this->timeStep);
+}
+
+void SPHFlameSolver::simulate(const float timeStep)
+{
+	std::vector<CSPHParticle*> particles;
+
+	for (auto fluid : flames) {
+		const auto ps = fluid->getParticles();
+		particles.insert(particles.end(), ps.begin(), ps.end());
+	}
+
+	SPHKernel kernel(effectLength);
+	for (auto p : particles) {
+		p->setKernel(&kernel);
+	}
+
+	//kernel.setEffectLength(fluids.front()->)
+
+	for (auto particle : particles) {
+		particle->init();
+	}
+
+	IndexedSortSearchAlgo algo(effectLength);
+	for (auto p : particles) {
+		algo.add(p);
+	}
+	algo.createPairs();
+	const auto& pairs = algo.getPairs();
+
+#pragma omp parallel for
+	for (int i = 0; i < static_cast<int>(pairs.size()); ++i) {
+		auto p1 = static_cast<CSPHParticle*>(pairs[i].first);
+		auto p2 = static_cast<CSPHParticle*>(pairs[i].second);
+		p1->addDensity(*p2);
+		p2->addDensity(*p1);
+	}
+
+	for (int i = 0; i < static_cast<int>(particles.size()); ++i) {
+		particles[i]->addSelfDensity();
+	}
+
+#pragma omp parallel for
+	for (int i = 0; i < static_cast<int>(pairs.size()); ++i) {
+		auto p1 = static_cast<CSPHParticle*>(pairs[i].first);
+		auto p2 = static_cast<CSPHParticle*>(pairs[i].second);
+		p1->solvePressureForce(*p2);
+		p2->solvePressureForce(*p1);
+		p1->solveViscosityForce(*p2);
+		p2->solveViscosityForce(*p1);
+	}
+
+	for (auto p : particles) {
+		p->addExternalForce(externalForce, timeStep);
+	}
+
+	//CSPHBoundarySolver boundarySolver(timeStep, boundary);
+	//boundarySolver.solve(particles);
+
+	for (auto p : particles) {
+		p->forwardTime(timeStep);
+	}
+
+	for (auto fluid : flames) {
+		fluid->getPresenter()->updateView();
+	}
+}
---
+++ CrystalPhysics/CrystalPhysics/SPHFlameSolver.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "SPHKernel.h"
+#include "../../Crystal/Math/Box3d.h"
+#include "../../Crystal/Scene/IAnimator.h"
+//#include "ISPHSolver.h"
+#include <vector>
+
+namespace Crystal {
+	namespace Physics {
+		class SPHFlameScene;
+
+class SPHFlameSolver : public Scene::IAnimator
+{
+public:
+	SPHFlameSolver() : timeStep(0.001f)
+	{}
+
+	void add(SPHFlameScene* flame) { this->flames.push_back(flame); }
+
+	void clear();
+
+	void step() override;
+
+	void simulate(const float timeStep);
+
+	void setTimeStep(const float timeStep) { this->timeStep = timeStep; }
+
+	//int getTimeStep() const { return timeStep; }
+
+	void setExternalForce(const Math::Vector3df& force) { this->externalForce = force; }
+
+	//std::vector<CSPHParticle*> getParticles() const { return particles; }
+
+	void setBoundary(const Math::Box3dd& boundary) { this->boundary = boundary; }
+
+	void setEffectLenth(const float length) { this->effectLength = length; }
+
+private:
+	float timeStep;
+	float effectLength = 1.25f;
+	//SPHKernel kernel;
+	std::vector<SPHFlameScene*> flames;
+	Math::Vector3df externalForce;
+	Math::Box3dd boundary;
+};
+
+	}
+}
