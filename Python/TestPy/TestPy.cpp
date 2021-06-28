38cd3df3f2da6e9351c9f6ece961bda9e476ef81
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2021-06-28 09:51:47 +0900

    add python sample.

---
+++ Python/TestPy/TestPy.cpp
@@ -0,0 +1,14 @@
+﻿// TestPy.cpp : スタティック ライブラリ用の関数を定義します。
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: これは、ライブラリ関数の例です
+#include <Python.h>
+
+static PyObject* c_helloworld(PyObject* self, PyObject* args)
+{
+    printf("Hello World\n");
+    return Py_None;
+}
