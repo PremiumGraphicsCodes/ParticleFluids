c7071b126e6702978128b52cbea32505d9107d61
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2021-09-09 13:29:55 +0900

    implement test view.

--- CrystalSpace/CrystalSpaceView/RayTracerView.cpp
+++ CrystalSpace/CrystalSpaceView/RayTracerView.cpp
@@ -26,7 +26,7 @@ void RayTracerView::onOk()
 	RayTracer rayTracer;
 	rayTracer.buildSpace(Box3dd(Vector3dd(0,0,0), Vector3dd(10,10,10)),4);
 
-	Box3dd b1(Vector3dd(8, 8, 8), Vector3dd(8, 8, 8));
+	Box3dd b1(Vector3dd(8, 8, 8), Vector3dd(9, 9, 9));
 	auto item1 = new LinearOctreeItem(b1);
 	rayTracer.add(item1);
 
@@ -39,12 +39,15 @@ void RayTracerView::onOk()
 	rayTracer.add(item3);
 
 	Ray3d ray(Vector3dd(0.1, 0.1, 0.1), Vector3dd(10, 10, 10));
-	auto found = rayTracer.trace(ray, 1.0);
+	auto found = rayTracer.trace(ray, 0.5);
+	found.sort();
 	found.unique();
-	/*
+
 	WireFrameBuilder wfBuilder;
-	for (auto b : boxes) {
-		wfBuilder.build(b);
+	for (auto f : found) {
+		for (auto i : f->getItems()) {
+			wfBuilder.build(i->getBoundingBox());
+		}
 	}
 
 	auto wf = wfBuilder.createWireFrame();
@@ -56,5 +59,4 @@ void RayTracerView::onOk()
 
 	auto presenter = scene->getPresenter();
 	presenter->createView(getWorld()->getRenderer(), *getWorld()->getGLFactory());
-	*/
 }
