38738634928c16b07223fea903a757c91fa1d3b4
mabuchi <mabuchi@premiumgraphics.co.jp>
2021-09-21 11:55:43 +0900

    add test view.

---
+++ CrystalViewer/AppBase/ITMAddView.cpp
@@ -0,0 +1,44 @@
+#include "ITMAddView.h"
+
+#include "../../Crystal/Scene/TriangleMeshScene.h"
+
+using namespace Crystal::Math;
+using namespace Crystal::Shape;
+using namespace Crystal::Scene;
+using namespace Crystal::UI;
+
+ITMAddView::ITMAddView(const std::string& name, World* model, Canvas* canvas) :
+	IOkCancelView(name, model, canvas),
+	nameView("Name", "PolygonMesh01")//,
+	//materialView("Material", model),
+	//presenterView("Presenter")
+{
+	add(&nameView);
+	//add(&materialView);
+	//add(&presenterView);
+}
+
+void ITMAddView::addPolygonMesh(const TriangleMesh& shape)
+{
+	/*
+	auto world = getWorld();
+	auto mesh = std::make_unique<PolygonMesh>();
+	mesh->positions = builder.getPositions();
+	mesh->normals = builder.getNormals();
+	mesh->texCoords = builder.getTexCoords();
+	mesh->vertices = builder.getVertices();
+	mesh->faces = builder.getFaces();
+
+	const auto materialName = materialView.getSelected();
+	auto material = world->getScenes()->findSceneByName<MaterialScene*>(materialName);
+
+	auto scene = new PolygonMeshScene(world->getNextSceneId(), nameView.getValue(), std::move(mesh));
+	PolygonMeshScene::FaceGroup group(builder.getFaces(), material);
+	scene->addGroup(group);
+	world->getScenes()->addScene(scene);
+
+	//presenterView.setPresenter(scene, world);
+
+	scene->getPresenter()->createView(getWorld()->getRenderer(), *getWorld()->getGLFactory());
+	*/
+}
---
+++ CrystalViewer/AppBase/ITMAddView.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "IOkCancelView.h"
+#include "StringView.h"
+//#include "TMPresenterView.h"
+
+namespace Crystal {
+	namespace Shape {
+		class TriangleMesh;
+	}
+	namespace UI {
+
+class ITMAddView : public IOkCancelView
+{
+public:
+	ITMAddView(const std::string& name, Scene::World* model, Canvas* canvas);
+
+protected:
+	void addPolygonMesh(const Shape::TriangleMesh& shape);
+
+private:
+	StringView nameView;
+	//TMPresenterView presenterView;
+};
+
+	}
+}
