726811d600b77239a84c3562a66570542597e61b
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2021-10-07 16:20:22 +0900

    split file.

--- Blender/CrystalPython/__init__.py
+++ Blender/CrystalPython/__init__.py
@@ -48,6 +48,7 @@ from ui.bl_particle_system import *
 from ui.bl_triangle_mesh import *
 from ui.bl_voxel import *
 from ui.model import Model as model
+from ui.mesh_to_ps_operator import MeshToPSOperator
 
 from bpy_extras.io_utils import (
     ImportHelper,
@@ -100,34 +101,6 @@ class ParticleSystemGenerator(bpy.types.Operator) :
       pc.ps.set_positions(positions)
       pc.convert_to_polygon_mesh("")      
       return {'FINISHED'}
-
-class MeshToPSOperator(bpy.types.Operator) :
-  bl_idname = "pg.meshtops"
-  bl_label = "MeshToPS"
-  bl_options = {"REGISTER", "UNDO"}
-
-  def execute(self, context) :
-      selected_mesh = self.get_selected_mesh(context)
-      mesh = BLTriangleMesh(model.scene)
-      mesh.convert_from_polygon_mesh(selected_mesh)
-      mesh.convert_to_polygon_mesh("hello")
-      voxel = BLVoxel(model.scene)
-      voxel.build()
-      voxelizer = Voxelizer(model.scene)
-      voxelizer.voxelize(mesh.mesh.id, voxel.voxel.id, 0.2)
-      values = voxel.voxel.get_values()
-      voxel.convert_to_polygon_mesh("voxel")
-      ps = BLParticleSystem(model.scene)
-      ps.ps.create_empty("")
-      voxel.voxel.convert_to_ps(ps.ps.id)
-      ps.convert_to_polygon_mesh("ps")
-      return {'FINISHED'}
-
-  def get_selected_mesh(self, context) :
-    for o in bpy.data.objects:
-      if o.type == 'MESH' and o.select_get():
-        return o.to_mesh()
-    return None
 
 class PSToMeshOperator(bpy.types.Operator) :
   bl_idname = "pg.pstomesh"
