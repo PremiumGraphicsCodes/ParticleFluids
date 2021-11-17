5c2feeb1f8e175845e597cde2dae4a0b7279bc79
mabuchi <mabuchi@premiumgraphics.co.jp>
2021-11-17 14:54:03 +0900

    fix interface.

--- Blender/CrystalPython/ui/particle_system_sequence_meshing_ui.py
+++ Blender/CrystalPython/ui/particle_system_sequence_meshing_ui.py
@@ -28,8 +28,7 @@ class MeshingRunner :
         prop = bpy.context.scene.meshing_property
         input_path = prop.input_path_prop
         output_path = prop.output_path_prop
-        cell_length = prop.cell_length_prop
-        effect_length = prop.effect_length_prop
+        particle_radius = prop.particle_radius_prop
 
         # TODO ファイル一覧の取得
         file_path = os.path.join(input_path, "test" + str(frame) + ".txt")
@@ -41,7 +40,7 @@ class MeshingRunner :
         mesh.create_empty("")
             
         builder = SurfaceBuilder(model.scene)
-        builder.build_anisotorpic(ps.id, mesh.id, cell_length, effect_length)
+        builder.build_anisotorpic(ps.id, mesh.id, particle_radius)
 
         basename_without_ext = os.path.splitext(os.path.basename(file_path))[0]
         export_file_path = os.path.join(output_path, basename_without_ext + ".stl")
@@ -112,22 +111,14 @@ class MeshingProperty(bpy.types.PropertyGroup) :
         subtype='DIR_PATH'
     )
         
-    cell_length_prop : bpy.props.FloatProperty(
-        name="cell_length",
-        description="CellLength",
+    particle_radius_prop : bpy.props.FloatProperty(
+        name="particle_radius",
+        description="ParticleRadius",
         default=1.0,
         min = 0.0,
         max = 100.0,
         )
         
-    effect_length_prop : bpy.props.FloatProperty(
-        name="effect_length",
-        description="EffectLength",
-        default=2.0,
-        min=0.0,
-        max=100.0,
-        )
-
 class ParticleSystemSequenceMeshingPanel(bpy.types.Panel):
     bl_label = "PSSeqMeshing"
     bl_space_type = 'VIEW_3D'
@@ -139,12 +130,11 @@ class ParticleSystemSequenceMeshingPanel(bpy.types.Panel):
         prop = context.scene.meshing_property
         self.layout.prop(prop, "input_path_prop", text="InputPath")
         self.layout.prop(prop, "output_path_prop", text="OutputPath")
-        self.layout.prop(prop, "cell_length_prop", text="CellLength")
-        self.layout.prop(prop, "effect_length_prop", text="EffectLength")
-        #if not runner.is_running() :
-        self.layout.operator(ParticleSystemSequenceMeshingOperator.bl_idname, text="Start", icon = "PLAY")
-        #else :
-        #    self.layout.operator(ParticleSystemSequenceMeshingOperator.bl_idname,text="Stop", icon='PAUSE')
+        self.layout.prop(prop, "particle_radius_prop", text="ParticleRadius")
+        if not runner.is_running() :
+            self.layout.operator(ParticleSystemSequenceMeshingOperator.bl_idname, text="Start", icon = "PLAY")
+        else :
+            self.layout.operator(ParticleSystemSequenceMeshingOperator.bl_idname, text="Stop", icon='PAUSE')
 
 classes = [
     ParticleSystemSequenceMeshingOperator,
