3aed2ea6a7c178cd3b31fb3f0cd142f4a9179b80
mabuchi <mabuchi@premiumgraphics.co.jp>
2021-11-24 11:36:06 +0900

    refactor.

--- Blender/CrystalPython/ui/bl_solver.py
+++ Blender/CrystalPython/ui/bl_solver.py
@@ -10,27 +10,21 @@ from scene.file_io import FileIO
 
 class BLSolver :
     def __init__(self) :
-        self.solver = None
+        self.__solver = None
         self.__running = False
         self.__bl_fluids = []
         self.__bl_boundaries = []
-        self.__frame = 1
-        self.external_force = Vector3df(0.0, 0.0, -9.8)
-        self.time_step = 0.01
-        self.export_dir_path = "tmp_txt"
+        self.__external_force = Vector3df(0.0, 0.0, -9.8)
+        self.__time_step = 0.01
+        self.__export_dir_path = "tmp_txt"
 
     def build(self):
-        if self.solver != None :
+        if self.__solver != None :
             return
 
-        self.solver = SolverScene(model.scene)
-        self.solver.create()
+        self.__solver = SolverScene(model.scene)
+        self.__solver.create()
         
-        #external_force = bpy.context.scene.solver_property.external_force_prop
-        #self.solver.external_force = Vector3df(external_force[0],external_force[1],external_force[2])
-
-        #self.solver.time_step = bpy.context.scene.solver_property.time_step_prop
-
     def add_fluid(self, bl_fluid) :
         self.__bl_fluids.append(bl_fluid)
 
@@ -41,14 +35,16 @@ class BLSolver :
         fluids = []
         for bl_fluid in self.__bl_fluids :
             fluids.append( bl_fluid.fluid )
-        self.solver.fluids = fluids
+        self.__solver.fluids = fluids
 
         boundaries = []
         for bl_boundary in self.__bl_boundaries :
             boundaries.append( bl_boundary.boundary )
-        self.solver.boundaries = boundaries
+        self.__solver.boundaries = boundaries
 
-        self.solver.send()
+        self.__solver.external_force = self.__external_force
+        self.__solver.time_step = self.__time_step
+        self.__solver.send()
 
     def start(self):
         self.__running = True
@@ -57,15 +53,12 @@ class BLSolver :
         self.__running = False
 
     def step(self, frame):
-        if frame == self.__frame :
-            return
-        self.solver.simulate()
+        self.__solver.simulate()
         for bl_fluid in self.__bl_fluids :
             bl_fluid.update()
         
-        file_path = os.path.join(self.export_dir_path, "test" + str(frame) + ".txt")
-        self.solver.export_txt(file_path)
-        self.__frame = frame
+        file_path = os.path.join(self.__export_dir_path, "test" + str(frame) + ".txt")
+        self.__solver.export_txt(file_path)
 
     def is_running(self):
         return self.__running
@@ -75,5 +68,3 @@ class BLSolver :
             bl_fluid.reset()
         self.__bl_fluids.clear()
         self.__bl_boundaries.clear()
-        self.__frame = 1
-
