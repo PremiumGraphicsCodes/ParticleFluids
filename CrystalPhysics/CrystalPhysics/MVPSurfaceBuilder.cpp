d0a3d2a3cf17ede104b837c26559770fe55fd1fd
mabuchi <mabuchi@premiumgraphics.co.jp>
2021-12-14 08:59:17 +0900

    refactor MVPSurfaceBuilder.

--- CrystalPhysics/CrystalPhysics/MVPSurfaceBuilder.cpp
+++ CrystalPhysics/CrystalPhysics/MVPSurfaceBuilder.cpp
@@ -15,42 +15,62 @@ void MVPSurfaceBuilder::build(const std::vector<MVPVolumeParticle*>& volumeParti
 	if (volumeParticles.empty()) {
 		return;
 	}
-	const auto radius = volumeParticles.front()->getRadius();
-	const auto tableSize = volumeParticles.size() * volumeParticles.front()->getMassParticles().size();
-	CompactSpaceHash3d spaceHash;
-	spaceHash.setup(radius, tableSize);
+	std::vector<IParticle*> vps;
+	std::vector<IParticle*> massParticles;
 	for (auto vp : volumeParticles) {
-		const auto& mps = vp->getMassParticles();
+		vps.push_back(vp);
+		const auto mps = vp->getMassParticles();
 		for (auto mp : mps) {
-			spaceHash.add(mp);
+			massParticles.push_back(mp);
 		}
 	}
+	const auto radius = volumeParticles.front()->getRadius();
+	buildVolumes(vps, radius, res);
+	buildMasses(massParticles, radius);
+	buildCells(threshold);
+}
+
+void MVPSurfaceBuilder::buildVolumes(const std::vector<IParticle*>& positions, const double radius, const int res)
+{
+	if (positions.empty()) {
+		return;
+	}
 	const auto r = radius;
-	const auto l = r * 2.0f;
+	//const auto l = r * 2.0f;
 
-	SparseVolumef sp(Vector3df(radius), tableSize);
+	const auto tableSize = static_cast<int>(positions.size());
+	this->sparseVolume = std::make_unique<SparseVolumef>(Vector3df(radius), tableSize);
 
-	const auto size = volumeParticles.size();
-//#pragma omp parallel for
-	for (int i = 0; i < size; ++i) {
-		auto vp = volumeParticles[i];
-		const auto index = sp.toIndex(vp->getPosition());
+	for (int i = 0; i < positions.size(); ++i) {
+		auto vp = positions[i];
+		const auto index = this->sparseVolume->toIndex(vp->getPosition());
 		for (int i = -res; i <= res; ++i) {
 			for (int j = -res; j <= res; ++j) {
 				for (int k = -res; k <= res; ++k) {
 					std::array<int, 3> ix = { index[0] + i, index[1] + j, index[2] + k };
-					if (!sp.exists(ix)) {
-						sp.createNode(ix);
+					if (!this->sparseVolume->exists(ix)) {
+						this->sparseVolume->createNode(ix);
 					}
 				}
 			}
 		}
 	}
+}
 
-	const auto nodes= sp.getNodes();
+void MVPSurfaceBuilder::buildMasses(const std::vector<IParticle*>& massParticles, const double radius)
+{
+	const int tableSize = massParticles.size();
+
+	CompactSpaceHash3d spaceHash;
+	spaceHash.setup(radius, tableSize);
+	for (auto mp : massParticles) {
+		spaceHash.add(mp);
+	}
+
+	const auto nodes = this->sparseVolume->getNodes();
 	for (auto node : nodes) {
 		const auto p = node->getPosition();
-		const auto count = float( spaceHash.findNeighbors(p).size() );
+		const auto count = float(spaceHash.findNeighbors(p).size());
 		node->setValue(count);
 	}
 
@@ -61,21 +81,21 @@ void MVPSurfaceBuilder::build(const std::vector<MVPVolumeParticle*>& volumeParti
 		const auto j = ii[1];
 		const auto k = ii[2];
 
-		std::array<std::array<int, 3>,8> indices;
+		std::array<std::array<int, 3>, 8> indices;
 		indices[0] = { i,j,k };
 		indices[1] = { i + 1,j,k };
 		indices[2] = { i + 1, j + 1,k };
 		indices[3] = { i,j + 1,k };
-		indices[4] = { i,j,k+1 };
-		indices[5] = { i + 1,j,k+1 };
-		indices[6] = { i + 1, j + 1,k+1 };
-		indices[7] = { i,j + 1,k+1 };
+		indices[4] = { i,j,k + 1 };
+		indices[5] = { i + 1,j,k + 1 };
+		indices[6] = { i + 1, j + 1,k + 1 };
+		indices[7] = { i,j + 1,k + 1 };
 
 		MCCell cell;
 		for (int i = 0; i < 8; ++i) {
-			const auto c = sp.findNode(indices[i]);
+			const auto c = this->sparseVolume->findNode(indices[i]);
 			if (c == nullptr) {
-				cell.vertices[i].position = sp.getPositionAt(indices[i]);
+				cell.vertices[i].position = this->sparseVolume->getPositionAt(indices[i]);
 				cell.vertices[i].value = 0.0f;
 			}
 			else {
@@ -86,101 +106,13 @@ void MVPSurfaceBuilder::build(const std::vector<MVPVolumeParticle*>& volumeParti
 				cell.vertices[i].value = c->getValue();
 			}
 		}
+		mcCells.push_back(cell);
+	}
+}
+
+void MVPSurfaceBuilder::buildCells(const double threshold)
+{
+	for (const auto& cell : mcCells) {
 		mc.march(cell, threshold);
-	}
-
-	/*
-	const auto size = volumeParticles.size();
-	//#pragma omp parallel for
-	for (int i = 0; i < size; ++i) {
-		auto vp = volumeParticles[i];
-		//for (auto vp : volumeParticles) {
-		const auto min = vp->getPositionf() - Vector3df(l, l, l);
-		const auto max = vp->getPositionf() + Vector3df(l, l, l);
-		const Box3df box(min, max);
-
-		const auto p = vp->getPosition();
-		const auto ix = int(p.x / radius);
-		const auto iy = int(p.y / radius);
-		const auto iz = int(p.z / radius);
-		std::array<int, 3> index = { ix, iy, iz };
-
-		Util::Array3d<MCCell::Vertex> grid(res * 2 + 1, res * 2 + 1, res * 2 + 1);
-		for (int i = -(int)res; i <= res; ++i) {
-			for (int j = -(int)res; j <= res; ++j) {
-				for (int k = -(int)res; k <= res; ++k) {
-					const auto p = spaceHash.toPosition({ ix + i, iy + j, iz + k });
-					const auto c = spaceHash.find(p);
-					MCCell::Vertex v;
-					v.position = p;
-					v.value = static_cast<double>(c.size());
-					grid.set(i + res, j + res, k + res, v);
-				}
-			}
-		}
-		for (int i = 0; i < res * 2; ++i) {
-			for (int j = 0; j < res * 2; ++j) {
-				for (int k = 0; k < res * 2; ++k) {
-					MCCell cell;
-					cell.vertices[0] = grid.get(i, j, k);
-					cell.vertices[1] = grid.get(i + 1, j, k);
-					cell.vertices[2] = grid.get(i + 1, j + 1, k);
-					cell.vertices[3] = grid.get(i, j + 1, k);
-					cell.vertices[4] = grid.get(i, j, k + 1);
-					cell.vertices[5] = grid.get(i + 1, j, k + 1);
-					cell.vertices[6] = grid.get(i + 1, j + 1, k + 1);
-					cell.vertices[7] = grid.get(i, j + 1, k + 1);
-					mc.march(cell, threshold);
-				}
-			}
-		}
-	}
-	*/
-}
-
-void MVPSurfaceBuilder::build(const std::vector<Vector3dd>& positions, const double radius, const double threshold)
-{
-	CompactSpaceHash3d spaceHash;
-	spaceHash.setup(radius, positions.size());
-	ParticleSystem<float> ps;
-	for (auto p : positions) {
-		ps.add(p, 0.0);
-	}
-	auto particles = ps.getIParticles();
-	for (auto p : particles) {
-		spaceHash.add(p);
-	}
-	build(spaceHash, threshold);
-}
-
-void MVPSurfaceBuilder::build(const CompactSpaceHash3d& hash, const double threshold)
-{
-	auto cells = hash.getCells();
-
-	for (auto c : cells) {
-		const std::array<int, 3> ii = c->index;
-		const auto i = ii[0];
-		const auto j = ii[1];
-		const auto k = ii[2];
-
-		std::array< std::array<int, 3>, 8> indices;
-		indices[0] = ii;
-		indices[1] = { i+1, j, k };
-		indices[2] = { i+1, j+1, k };
-		indices[3] = { i, j+1, k };
-		indices[4] = { i, j, k + 1 };
-		indices[5] = { i + 1, j, k + 1 };
-		indices[6] = { i + 1, j + 1, k + 1 };
-		indices[7] = { i, j + 1, k + 1 };
-
-		MCCell mcCell;
-		for (int u = 0; u < 8; ++u) {
-			const auto index = indices[u];
-			const auto cc0 = hash.find(index);
-			const auto w0 = static_cast<double>( cc0.size() );
-			mcCell.vertices[u].position = hash.toPosition(index);
-			mcCell.vertices[u].value = w0;
-		}
-		mc.march(mcCell, threshold);
 	}
 }
--- CrystalPhysics/CrystalPhysics/MVPSurfaceBuilder.h
+++ CrystalPhysics/CrystalPhysics/MVPSurfaceBuilder.h
@@ -17,17 +17,20 @@ class MVPSurfaceBuilder
 public:
 	void build(const std::vector<MVPVolumeParticle*>& particles, const int res, const double threshold);
 
-	void build(const std::vector<Math::Vector3dd>& positions, const double radius, const double threshold);
+	void buildVolumes(const std::vector<Shape::IParticle*>& volumeParticles, const double radius, const int res);
 
-	void build(const Space::CompactSpaceHash3d& hash, const double threshold);
+	void buildMasses(const std::vector<Shape::IParticle*>& massParticles, const double radius);
+
+	void buildCells(const double threshold);
 
 	//float getVolumeAt(const Math::Vector3df& pos);
 
 	std::vector<Math::Triangle3d> getTriangles() const { return mc.getTriangles(); }
 
 private:
+	std::unique_ptr<Space::SparseVolumef> sparseVolume;
 	Space::MarchingCubesAlgo mc;
-	//std::vector<Space::MCCell> mcCells;
+	std::vector<Space::MCCell> mcCells;
 
 };
 	}
