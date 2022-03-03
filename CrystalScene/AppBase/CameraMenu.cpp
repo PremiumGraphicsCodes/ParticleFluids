2fa0716e2393fe4904c3315dfe72f46f477ef6f1
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-03-03 13:01:29 +0900

    refactor.

--- CrystalScene/AppBase/CameraMenu.cpp
+++ CrystalScene/AppBase/CameraMenu.cpp
@@ -11,82 +11,35 @@ using namespace Crystal::Scene;
 using namespace Crystal::UI;
 using namespace Crystal::Command;
 
-namespace {
-	class FitMenuItem : public IMenuItem
-	{
-	public:
-		FitMenuItem(const std::string& name, World* model) :
-			IMenuItem(name),
-			model(model)
-		{}
-
-		void onPushed() override {
-			CameraFitCommand command;
-			command.execute(model);
-		}
-
-	private:
-		World* model;
-	};
-
-	class XYMenuItem : public IMenuItem
-	{
-	public:
-		XYMenuItem(const std::string& name, World* model) :
-			IMenuItem(name),
-			model(model)
-		{}
-
-		void onPushed() override {
-			CameraXYCommand command;
-			command.execute(model);
-		}
-
-	private:
-		World* model;
-	};
-
-	class YZMenuItem : public IMenuItem
-	{
-	public:
-		YZMenuItem(const std::string& name, World* model) :
-			IMenuItem(name),
-			model(model)
-		{}
-
-		void onPushed() override {
-			CameraYZCommand command;
-			command.execute(model);
-		}
-
-	private:
-		World* model;
-	};
-
-	class ZXMenuItem : public IMenuItem
-	{
-	public:
-		ZXMenuItem(const std::string& name, World* model) :
-			IMenuItem(name),
-			model(model)
-		{}
-
-		void onPushed() override {
-			CameraZXCommand command;
-			command.execute(model);
-		}
-
-	private:
-		World* model;
-	};
-
-}
-
 CameraMenu::CameraMenu(const std::string& name, World* model, Canvas* canvas) :
 	IMenu(name, model, canvas)
 {
-	add(new FitMenuItem("Fit", model));
-	add(new XYMenuItem("XY", model));
-	add(new YZMenuItem("YZ", model));
-	add(new ZXMenuItem("XZ", model));
+	add(new MenuItem("Fit", [&] { onFit(); }));
+	add(new MenuItem("XY", [&] { onXY(); }));
+	add(new MenuItem("YZ", [&] { onYZ(); }));
+	add(new MenuItem("ZX", [&] { onZX(); }));
+}
+
+void CameraMenu::onFit()
+{
+	CameraFitCommand command;
+	command.execute(getWorld());
+}
+
+void CameraMenu::onXY()
+{
+	CameraXYCommand command;
+	command.execute(getWorld());
+}
+
+void CameraMenu::onYZ()
+{
+	CameraYZCommand command;
+	command.execute(getWorld());
+}
+
+void CameraMenu::onZX()
+{
+	CameraZXCommand command;
+	command.execute(getWorld());
 }
--- CrystalScene/AppBase/CameraMenu.h
+++ CrystalScene/AppBase/CameraMenu.h
@@ -9,6 +9,15 @@ class CameraMenu : public IMenu
 {
 public:
 	CameraMenu(const std::string& name, Scene::World* model, Canvas* canvas);
+
+private:
+	void onFit();
+
+	void onXY();
+
+	void onYZ();
+
+	void onZX();
 };
 
 	}
