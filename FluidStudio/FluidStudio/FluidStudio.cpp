293372e108bfb258500411529be5ae7c0cbb9846
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-04-11 09:02:41 +0900

    add project.

--- FluidStudio/FluidStudio/FluidStudio.cpp
+++ FluidStudio/FluidStudio/FluidStudio.cpp
@@ -1,20 +1,51 @@
-﻿// FluidStudio.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿#include "CrystalScene/AppBase/Window.h"
+#include "CrystalScene/AppBase/FileMenu.h"
+#include "CrystalScene/AppBase/CameraMenu.h"
+#include "CrystalScene/AppBase/CtrlMenu.h"
+#include "CrystalScene/AppBase/ShapeMenu.h"
+#include "CrystalScene/AppBase/AppearanceMenu.h"
+#include "CrystalScene/AppBase/SceneListPanel.h"
+#include "CrystalScene/AppBase/Canvas.h"
+#include "CrystalScene/AppBase/ControlPanel.h"
+
+#include "CrystalScene/Scene/World.h"
 
 #include <iostream>
+#include <cassert>
+
+using namespace Crystal::Math;
+using namespace Crystal::Graphics;
+using namespace Crystal::Scene;
+using namespace Crystal::UI;
+
+using namespace Crystal::Shader;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	World world;
+	Canvas canvas;
+
+	Window window("FluidStudio", &world, &canvas);
+	if (!window.init()) {
+		std::cout << "window initialization failed!" << std::endl;
+		assert(false);
+		return 0;
+	}
+
+	auto control = new ControlPanel("Control", &world, &canvas);
+	window.add(control);
+
+	window.add(new FileMenu("File", &world, &canvas));
+	window.add(new CameraMenu("Camera", &world, &canvas));
+	window.add(new CtrlMenu("Ctrl", &world, &canvas));
+	window.add(new ShapeMenu("Shape", &world, &canvas, control));
+	window.add(new AppearanceMenu("Appearance", &world, &canvas, control));
+	//window.add(new PhysicsMenu("Physics", &world, &canvas, control));
+
+	window.add(new SceneListPanel("Scene", &world, &canvas, control));
+
+	window.show();
+
+	return 0;
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
