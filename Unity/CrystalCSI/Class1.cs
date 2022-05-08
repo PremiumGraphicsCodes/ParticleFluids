97706a968e0a410bbd64c8f73c197e852f0f4456
mabuchi <mabuchi@premiumgraphics.co.jp>
2022-05-09 08:50:52 +0900

    add sample.

---
+++ Unity/CrystalCSI/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace CrystalCSI
+{
+
+    public class CSI{
+        // 値渡し
+        [DllImport("Unity")]
+        public static extern int SampleAPIInt(int i);
+        
+        [DllImport("Unity")]
+        public static extern float SampleAPIFloat(float f);
+        
+        [DllImport("Unity")]
+        public static extern double SampleAPIDouble(double d);
+
+        // 参照渡し
+        [DllImport("Unity")]
+        public static extern void SampleAPIInt2(ref int i);
+
+        [DllImport("Unity")]
+        public static extern void SampleAPIFloat2(ref float f);
+
+        [DllImport("Unity")]
+        public static extern void SampleAPIDouble2(ref double d);
+
+        /*
+    // 配列の参照渡し
+    [DllImport("NativePluginSample")]
+    public static extern void SampleAPIIntArray(int[] intArray, int intArraySize);
+    [DllImport("NativePluginSample")]
+    public static extern void SampleAPILongArray(int[] longArray, int longArraySize);
+    [DllImport("NativePluginSample")]
+    public static extern void SampleAPIFloatArray(float[] floatArray, int floatArraySize);
+    [DllImport("NativePluginSample")]
+    public static extern void SampleAPIDoubleArray(double[] doubleArray, int doubleArraySize);
+
+    // 文字列
+    [DllImport("NativePluginSample")]
+    public static extern string SampleAPIString1();
+    [DllImport("NativePluginSample")]
+    public static extern string SampleAPIString2(string str);
+    */
+    }
+}
---
+++ Unity/CrystalCSITest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrystalCSI;
+
+namespace CrystalCSITest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var i = CSI.SampleAPIInt(1);
+            var f = CSI.SampleAPIFloat(1.0f);
+            var d = CSI.SampleAPIDouble(2.0);
+        }
+    }
+}
